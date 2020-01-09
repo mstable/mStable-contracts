7bf9d7b7845f81716cd0791b6b495f2fe1187ddd
alsco77 <alsco77@pm.me>
2020-01-09 14:17:40 +0100

    Added specific Masset implementations

---
+++ contracts/masset/mUSD.sol
@@ -0,0 +1,33 @@
+pragma solidity ^0.5.12;
+pragma experimental ABIEncoderV2;
+import { Masset } from "./Masset.sol";
+
+/**
+  * @title mUSD
+  * @author Stability Labs Pty Ltd
+  * @dev Base layer functionality for the Masset
+  */
+contract MUSD is Masset {
+
+    /** @dev constructor */
+    constructor (
+        address[] memory _bassets,
+        bytes32[] memory _bassetKeys,
+        uint256[] memory _bassetWeights,
+        address _feePool,
+        address _manager
+    )
+        Masset(
+            "mStable USD",
+            "mUSD",
+            _bassets,
+            _bassetKeys,
+            _bassetWeights,
+            new uint256[](0),
+            _feePool,
+            _manager
+        )
+        public
+    {
+    }
+}
