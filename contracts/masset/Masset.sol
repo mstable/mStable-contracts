ecdd3ed8b6d5b63d9dc96373cc0ab8cfb274c5b8
alsco77 <alsco77@pm.me>
2020-01-22 13:49:10 +0100

    Corrected mintTo redeemTo funcs

--- contracts/masset/Masset.sol
+++ contracts/masset/Masset.sol
@@ -65,18 +65,16 @@ contract Masset is IMasset, MassetToken, MassetBasket {
         basketIsHealthy
         returns (uint256 massetMinted)
     {
-        return mintTo(_bassetQuantity, msg.sender, msg.sender);
+        return mintTo(_bassetQuantity, msg.sender);
     }
 
     /**
       * @dev Mints a number of Massets based on the sum of the value of the Bassets
       * @param _bassetQuantity Exact units of Bassets to mint
-      * @param _minter Address from which to transfer the Bassets
       * @param _recipient Address to which the Masset should be minted
       */
     function mintTo(
         uint256[] memory _bassetQuantity,
-        address _minter,
         address _recipient
     )
         public
@@ -93,7 +91,7 @@ contract Masset is IMasset, MassetToken, MassetBasket {
             address basset = basket.bassets[i].addr;
 
             if(_bassetQuantity[i] > 0){
-                IERC20(basset).transferFrom(_minter, address(this), _bassetQuantity[i]);
+                IERC20(basset).transferFrom(msg.sender, address(this), _bassetQuantity[i]);
 
                 basket.bassets[i].vaultBalance = basket.bassets[i].vaultBalance.add(_bassetQuantity[i]);
 
@@ -120,18 +118,16 @@ contract Masset is IMasset, MassetToken, MassetBasket {
         public
         returns (uint256 massetRedeemed)
     {
-        return redeemTo(_bassetQuantity, msg.sender, msg.sender);
+        return redeemTo(_bassetQuantity, msg.sender);
     }
 
     /**
       * @dev Redeems a certain quantity of Bassets, in exchange for burning the relative Masset quantity from the User
       * @param _bassetQuantity Exact quantities of Bassets to redeem
-      * @param _redeemer Account from which to burn the Masset
       * @param _recipient Account to which the redeemed Bassets should be sent
       */
     function redeemTo(
         uint256[] memory _bassetQuantity,
-        address _redeemer,
         address _recipient
     )
         public
@@ -153,13 +149,13 @@ contract Masset is IMasset, MassetToken, MassetBasket {
         }
 
         // Pay the redemption fee
-        _payRedemptionFee(massetQuantity, _redeemer);
+        _payRedemptionFee(massetQuantity, msg.sender);
 
         // Ensure payout is relevant to collateralisation ratio (if ratio is 90%, we burn more)
         massetQuantity = massetQuantity.divPrecisely(basket.collateralisationRatio);
 
         // Burn the Masset
-        _burn(_redeemer, massetQuantity);
+        _burn(msg.sender, massetQuantity);
 
         // Transfer the Bassets to the user
         for(uint i = 0; i < _bassetQuantity.length; i++){
@@ -170,7 +166,7 @@ contract Masset is IMasset, MassetToken, MassetBasket {
             }
         }
 
-        emit Redeemed(_recipient, _redeemer, massetQuantity, _bassetQuantity);
+        emit Redeemed(_recipient, msg.sender, massetQuantity, _bassetQuantity);
         return massetQuantity;
     }
 
