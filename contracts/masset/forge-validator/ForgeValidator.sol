ad91439241893b224314590233ac4e389936f702
alsco77 <alsco77@pm.me>
2020-02-10 18:19:07 +0100

    Massively optimised the ForgeValidator for redemptions

--- contracts/masset/forge-validator/ForgeValidator.sol
+++ contracts/masset/forge-validator/ForgeValidator.sol
@@ -16,7 +16,7 @@ contract ForgeValidator is IForgeValidator {
       * @dev Checks whether a given mint is valid
       */
     function validateMint(uint256 _totalVault, Basset memory _basset, uint256 _bassetQuantity)
-    public
+    external
     pure {
         require(_basset.status != BassetStatus.BrokenBelowPeg &&
             _basset.status != BassetStatus.Liquidating, "Basset not allowed in mint");
@@ -35,7 +35,7 @@ contract ForgeValidator is IForgeValidator {
       * @dev Checks whether a given mint is valid
       */
     function validateMint(uint256 _totalVault, Basset[] memory _bassets, uint256[] memory _bassetQuantity)
-    public
+    external
     pure {
         uint256 bassetCount = _bassets.length;
         require(bassetCount == _bassetQuantity.length, "indexes & _bAssetQty length should be equal");
@@ -66,81 +66,74 @@ contract ForgeValidator is IForgeValidator {
         }
     }
 
+
+    /**
+      * @dev Checks whether a given mint is valid
+      */
+    function validateRedemption(
+        Basset[] memory _allBassets,
+        bool basketIsFailed,
+        uint256 _totalVault,
+        uint256 _indexToRedeem,
+        uint256 _bassetQuantity
+    )
+        external
+        pure
+    {
+        Basset memory bAsset = _allBassets[_indexToRedeem];
+        require(bAsset.status != BassetStatus.BrokenAbovePeg || basketIsFailed, "Cannot redeem this bAsset unless Basket is failed");
+        // require(bAsset.vaultBalance >= _bassetQuantity, "Insufficient vault balance"); // This gets checked implicity through sub
+
+        (bool[] memory overweightBassetsBefore, bool atLeastOneOverweightBefore) = _getOverweightBassets(_totalVault, _allBassets);
+
+        _allBassets[_indexToRedeem].vaultBalance = bAsset.vaultBalance.sub(_bassetQuantity);
+
+        if(atLeastOneOverweightBefore){
+            // If the bAsset is broken above peg.. it doesn't count
+            require(overweightBassetsBefore[_indexToRedeem], "Must redeem overweight bAssets");
+        } else {
+            (, bool atLeastOneOverweightAfter) = _getOverweightBassets(_totalVault, _allBassets);
+            require(!atLeastOneOverweightAfter, "Redemption cannot push bAssets overweight");
+        }
+    }
+
     /**
       * @dev Checks whether a given redemption is valid
       * @param _bassets          Array of bassets
       * @param _bassetQuantity  Array of basset quantities to use in the redemption
       */
-    function validateRedemption(bool isBasketFailed, Basset[] memory _bassets, uint256[] memory _bassetQuantity)
-    public
-    pure {
-        uint256 bassetCount = _bassets.length;
-        require(_bassetQuantity.length == bassetCount, "Must provide values for all Bassets in system");
+    function validateRedemption(
+        Basset[] memory _allBassets,
+        bool basketIsFailed,
+        uint256 _totalVault,
+        uint256[] memory _idxs,
+        uint256[] memory _bassetQuantity
+    )
+        external
+        pure
+    {
+        uint idxCount = _idxs.length;
+        require(idxCount == _bassetQuantity.length, "Must provide values for all Bassets in system");
 
-        uint256 bassetsToRedeem = 0;
-        uint256 isolatedBassets = 0;
-        uint256 totalIsolatedWeightings = 0;
+        (bool[] memory overweightBassetsBefore, bool atLeastOneOverweightBefore) = _getOverweightBassets(_totalVault, _allBassets);
 
-        // Reformat basket to exclude affected Bassets and allow for relative calculations
-        for(uint i = 0; i < bassetCount; i++){
-            BassetStatus status = _bassets[i].status;
-            require(status != BassetStatus.Liquidating, "Basket cannot be undergoing liquidation");
-            bool isIsolated = isBasketFailed ? false : status == BassetStatus.BrokenAbovePeg;
-            if(isIsolated){
-                // If the Basset is supposed to be isolated from redemtion, ignore it from the basket
-                require(_bassetQuantity[i] == 0, "Cannot redeem isolated Bassets");
+        for(uint i = 0; i < idxCount; i++){
+            require(_allBassets[_idxs[i]].status != BassetStatus.BrokenAbovePeg || basketIsFailed,
+                "Cannot redeem depegged bAsset unless Basket is failed");
+            _allBassets[_idxs[i]].vaultBalance = _allBassets[_idxs[i]].vaultBalance.sub(_bassetQuantity[i]);
+        }
 
-                // Total the Isolated weightings and redistribute to non-affected assets
-                isolatedBassets += 1;
-                totalIsolatedWeightings = totalIsolatedWeightings.add(_bassets[i].maxWeight);
-
-                _bassets[i].maxWeight = 0;
-                _bassets[i].vaultBalance = 0;
-            } else if (_bassetQuantity[i] > 0) {
-                bassetsToRedeem += 1;
-                require(_bassets[i].vaultBalance >= _bassetQuantity[i], "Vault must have sufficient balance to redeem");
+        if(atLeastOneOverweightBefore){
+            //  Note, there is an edge case where redeeming these Bassets may push others above weightings, however
+            //  it is a side effect of simplicity
+            for(uint j = 0; j < idxCount; j++){
+                require(overweightBassetsBefore[_idxs[j]], "Must redeem overweight bAssets");
             }
-        }
-
-        require(bassetsToRedeem > 0, "Must choose some Bassets to redeem");
-
-        uint256[] memory preBassets = _getBassetWeightings(_bassets);
-
-        bool isInAdjustment = false;
-
-        // If totalIsolatedWeightings = 80e16, and there are 2 bassets un-isolated - given them 40 each
-        uint256 redistributedWeighting = isolatedBassets > 0 ? totalIsolatedWeightings.div(bassetCount.sub(isolatedBassets)) : 0;
-        for (uint k = 0; k < bassetCount; k++) {
-            uint maxWeight = _bassets[k].maxWeight.add(redistributedWeighting);
-            isInAdjustment = isInAdjustment || preBassets[k] > maxWeight;
-        }
-
-        // Theoretically redeem the bassets from the vault
-        for (uint j = 0; j < bassetCount; j++) {
-            _bassets[j].vaultBalance = _bassets[j].vaultBalance.sub(_bassetQuantity[j]);
-        }
-
-        uint256[] memory postBassets = _getBassetWeightings(_bassets);
-
-        // Check that the forge is valid, given the relative target weightings and vault balances
-        // isInAdjustment?
-        //     Bassets redeemed must be overweight
-        //     Note, there is an edge case where redeeming these Bassets may push others above weightings, however
-        //     it is a side effect of simplicity
-        // no
-        //     all unredeemed bassets must remain underweight
-        for (uint m = 0; m < bassetCount; m++){
-            uint maxWeight = _bassets[m].maxWeight.add(redistributedWeighting);
-            if(isInAdjustment){
-                if(_bassetQuantity[m] > 0) {
-                    require(preBassets[m] > maxWeight, "Redeemed Bassets must be overweight during adjustments");
-                }
-            } else {
-                require(postBassets[m] <= maxWeight, "Unredeemed bassets must stay below max weighting");
-            }
+        } else {
+            (, bool atLeastOneOverweightAfter) = _getOverweightBassets(_totalVault, _allBassets);
+            require(!atLeastOneOverweightAfter, "Redemption cannot push bAssets overweight");
         }
     }
-
 
     /***************************************
                     HELPERS
@@ -151,51 +144,26 @@ contract ForgeValidator is IForgeValidator {
       * @param _bassets Array of bassets
       * @return uint256[] Relative weightings of all Bassets where 100% == 1e18
       */
-    function _getBassetWeightings(Basset[] memory _bassets)
+    function _getOverweightBassets(uint256 _total, Basset[] memory _bassets)
     private
     pure
-    returns (uint256[] memory relativeWeights) {
-        uint256[] memory ratioedBassets = _getRatioedBassets(_bassets);
-
-        uint256 sumOfRatioedBassets = 0;
-        for(uint i = 0; i < ratioedBassets.length; i++) {
-            sumOfRatioedBassets = sumOfRatioedBassets.add(ratioedBassets[i]);
-        }
-
-        uint256 len = ratioedBassets.length;
-        relativeWeights = new uint256[](len);
-
-        for(uint256 i = 0; i < len; i++) {
-            if(sumOfRatioedBassets == 0){
-                relativeWeights[i] = _bassets[i].maxWeight;
-                continue;
-            }
-            if(ratioedBassets[i] == 0) {
-                relativeWeights[i] = 0;
-                continue;
-            }
-
-            relativeWeights[i] = ratioedBassets[i].divPrecisely(sumOfRatioedBassets);
-        }
-
-        return relativeWeights;
-    }
-
-    /**
-      * @dev Gets the value of all Bassets in Masset terms, using the given ratio
-      * @param _bassets    Array of bassets
-      * @return uint256[] Of Ratioed Assets
-      */
-    function _getRatioedBassets(Basset[] memory _bassets)
-    private
-    pure
-    returns (uint256[] memory ratioedAssets) {
+    returns (bool[] memory isOverWeight, bool atLeastOneOverweight) {
         uint256 len = _bassets.length;
+        isOverWeight = new bool[](len);
+        atLeastOneOverweight = false;
 
-        ratioedAssets = new uint256[](len);
         for(uint256 i = 0; i < len; i++) {
-            // If the Basset is isolated, it will have already been excluded from this calc
-            ratioedAssets[i] = _bassets[i].vaultBalance.mulRatioTruncate(_bassets[i].ratio);
+            BassetStatus status = _bassets[i].status;
+            require(status != BassetStatus.Liquidating, "bAssets undergoing liquidation");
+
+            uint256 ratioedBasset = _bassets[i].vaultBalance.mulRatioTruncate(_bassets[i].ratio);
+            uint256 maxWeightInUnits = _bassets[i].maxWeight.mulTruncate(_total);
+
+            // If the bAsset is de-pegged on the up-side, it doesn't matter if it goes above max
+            bool bassetOverWeight = ratioedBasset > maxWeightInUnits && status != BassetStatus.BrokenAbovePeg;
+            isOverWeight[i] = bassetOverWeight;
+
+            atLeastOneOverweight = atLeastOneOverweight || bassetOverWeight;
         }
     }
 }
