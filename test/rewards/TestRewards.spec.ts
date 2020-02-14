44d2c376d280a0b83fa3ab7c13e601fdc068126a
Jitendra Chittoda <jcchittoda@gmail.com>
2020-02-14 13:31:16 +0530

    Calling mint functions from Rewards contract (#14)
    * Calling mint functions from Reward contract.
    
    * Fix runtime issue
    
    * Approve bAsset tokens from Rewards contract.
    
    * Update tslint
    
    * Rewards approval function only be called by governer
    
    * Tidy up the code and improvement in `convertBitmapToBassets` function
    
    * Add test cases for Rewards contract.
    
    * Tweak expectevent
    
    * Fixed getter functions of Reward contract
    
    * Removed ReentrancyGuard from Rewards contract
    
    * Used Ownable instead of Governor modifier
    
    * Added more test cases and invariants check.
    
    * Updated openzeppelin-test-helpers to use the correct BN. Still not working.
    
    * Fixed Rewards test cases
    
    * .vscode: removed tab size 2. It will use tab size 4
    
    * Updated mint single test
    
    * Tidy up the code with Alex comments
    
    Co-authored-by: Alex Scott <5690430+alsco77@users.noreply.github.com>

--- test/rewards/TestRewards.spec.ts
+++ test/rewards/TestRewards.spec.ts
@@ -1,26 +1,33 @@
+
 import { createMultiple, percentToWeight, simpleToExactAmount } from "@utils/math";
 import { createBasket, createBasset, Basket } from "@utils/mstable-objects";
-import { shouldFail } from "openzeppelin-test-helpers";
+import { constants, expectEvent, shouldFail } from "openzeppelin-test-helpers";
 import { BassetMachine, MassetMachine, StandardAccounts, SystemMachine } from "@utils/machines";
 import { aToH, BN } from "@utils/tools";
 
+
 import envSetup from "@utils/env_setup";
 import * as chai from "chai";
-import { ERC20MockInstance, MassetInstance } from "types/generated";
+import { ERC20MockInstance, MassetInstance, ForgeRewardsMUSDInstance } from "types/generated";
 
 const Masset = artifacts.require("Masset");
+const ForgeRewardsMUSD = artifacts.require("ForgeRewardsMUSD");
+
 
 envSetup.configure();
 const { expect, assert } = chai;
 
 contract("Rewards", async (accounts) => {
-    const sa = new StandardAccounts(accounts);
+
+    let sa = new StandardAccounts(accounts);
     let systemMachine: SystemMachine;
     let masset: MassetInstance;
     // tslint:disable-next-line:one-variable-per-declaration
     let b1, b2, b3, b4, b5, b6, b7;
+    let rewardsContract: ForgeRewardsMUSDInstance;
 
-    before("Init contract", async () => {
+    beforeEach("Init contract", async () => {
+        //rewardContract = await deployer.deployed(c_ForgeRewardsMUSD);
         systemMachine = new SystemMachine(accounts, sa.other);
         await systemMachine.initialiseMocks();
         const bassetMachine = new BassetMachine(sa.default, sa.other, 500000);
@@ -62,21 +69,207 @@ contract("Rewards", async (accounts) => {
             sa.feePool,
             systemMachine.forgeValidator.address,
         );
+
+        // 3. Deploy ForgeRewardsMUSD
+        rewardsContract = await ForgeRewardsMUSD.new(
+            masset.address,
+            systemMachine.systok.address,
+            sa.governor,
+            { from: sa.governor }
+        );
     });
 
-    describe("Minting via Rewards contract", () => {
-        it("Governer should add rewards token to contract", async () => {});
+    describe("Contract deployed", async () => {
+        it("Should have valid parameters", async () => {
+            assert((await rewardsContract.mUSD()) === masset.address);
+            assert((await rewardsContract.MTA()) === systemMachine.systok.address);
+            assert((await rewardsContract.owner()) === sa.governor);
+        });
 
-        it("Should approve single bAsset", async () => {});
+        it("Should approved all bAsset tokens to max", async () => {
+            let MAX: BN = ((new BN(2)).pow(new BN(256))).sub(new BN(1)); // 2^256-1
+            assert((await b1.allowance(rewardsContract.address, masset.address)).eq(MAX));
+            assert((await b2.allowance(rewardsContract.address, masset.address)).eq(MAX));
+            assert((await b3.allowance(rewardsContract.address, masset.address)).eq(MAX));
+            assert((await b4.allowance(rewardsContract.address, masset.address)).eq(MAX));
+            assert((await b5.allowance(rewardsContract.address, masset.address)).eq(MAX));
+            assert((await b6.allowance(rewardsContract.address, masset.address)).eq(MAX));
+            assert((await b7.allowance(rewardsContract.address, masset.address)).eq(MAX));
 
-        it("Should approve multiple bAssets", async () => {});
+        });
+    });
 
-        it("Should mint single bAsset", async () => {});
+    describe("getTrancheData()", () => {
+        it("Should have initial Tranche data");
+    });
 
-        it("Should mint multiple bAsset", async () => {});
+    describe("approveAllBassets()", () => {
+        it("Should fail when called by non Governer");
+        it("Should allowed when called by Governer");
+        it("Should approve to MAX when allowances are utilized")
+    });
 
-        it("User should claim reward", async () => {});
+    describe("approveFor()", () => {
+        it("Should fail when called by non Governer");
+        it("Should allowed when called by Governer");
+        it("Should approve to MAX when allowances are utilized");
+    });
 
-        it("User should redeem reward", async () => {});
+    describe("mintTo()", () => {
+        it("Should mint single bAsset", async () => {
+            const newSystemMachine = new SystemMachine(accounts, sa.other);
+            await newSystemMachine.initialiseMocks();
+
+            const newMasset = await Masset.new(
+                "TestMasset",
+                "TMT",
+                newSystemMachine.nexus.address,
+                [b1.address],
+                [aToH("b1")],
+                [percentToWeight(100)],
+                [createMultiple(1)],
+                sa.feePool,
+                newSystemMachine.forgeValidator.address,
+            );
+
+            // 3. Deploy ForgeRewardsMUSD
+            const newRewardsContract = await ForgeRewardsMUSD.new(
+                newMasset.address,
+                newSystemMachine.systok.address,
+                sa.governor,
+                { from: sa.governor }
+            );
+
+            await b1.approve(newRewardsContract.address, 10, { from: sa.default });
+            assert((await b1.allowance(sa.default, newRewardsContract.address)).eq(new BN(10)));
+
+            const txReceipt = await newRewardsContract.mintSingleTo(b1.address, 10, sa.default, sa.default, { from: sa.default });
+            expectEvent.inLogs(txReceipt.logs, "MintVolumeIncreased", { trancheNumber: new BN(0), mintVolume: new BN(10) });
+            expectEvent.inLogs(txReceipt.logs, "RewardeeMintVolumeIncreased", { trancheNumber: new BN(0), rewardee: sa.default, mintVolume: new BN(10) });
+
+            assert((await newMasset.balanceOf(sa.default)).eq(new BN(10)));
+            assert((await newMasset.totalSupply()).eq(new BN(10)));
+
+            assert((await b1.balanceOf(newMasset.address)).eq(new BN(10)));
+            // Rewards updated
+            let data: any = await newRewardsContract.getTrancheData(0);
+            const rewardStartTime: BN = await newRewardsContract.rewardStartTime();
+            validateTrancheDates(data, rewardStartTime, 0);
+
+            assert(data.totalMintVolume.eq(new BN(10)));
+            assert(data.totalRewardUnits.eq(new BN(0)));
+            assert(data.unclaimedRewardUnits.eq(new BN(0)));
+
+            // Reward for the user
+            data = await newRewardsContract.getRewardeesData(0, [sa.default]);
+            assert(data.mintVolume[0].eq(new BN(10)));
+            assert(data.claimed[0] === false);
+            assert(data.rewardAllocation[0].eq(new BN(0)));
+            assert(data.redeemed[0] === false);
+
+        });
+
+        it("Should mint multiple bAssets", async () => {
+            await b1.approve(rewardsContract.address, 10, { from: sa.default });
+            await b2.approve(rewardsContract.address, 10, { from: sa.default });
+            await b3.approve(rewardsContract.address, 10, { from: sa.default });
+            await b4.approve(rewardsContract.address, 10, { from: sa.default });
+
+            assert((await b1.allowance(sa.default, rewardsContract.address)).eq(new BN(10)));
+            assert((await b2.allowance(sa.default, rewardsContract.address)).eq(new BN(10)));
+            assert((await b3.allowance(sa.default, rewardsContract.address)).eq(new BN(10)));
+            assert((await b4.allowance(sa.default, rewardsContract.address)).eq(new BN(10)));
+
+            const bitmap = 15; // 1111
+            const txReceipt = await rewardsContract.mintTo(
+                bitmap,
+                [10, 10, 10, 10],
+                sa.default,
+                sa.default,
+                { from: sa.default }
+            );
+
+            // Expect event
+            expectEvent.inLogs(txReceipt.logs, "MintVolumeIncreased", { trancheNumber: new BN(0), mintVolume: new BN(40) });
+            expectEvent.inLogs(txReceipt.logs, "RewardeeMintVolumeIncreased", { trancheNumber: new BN(0), rewardee: sa.default, mintVolume: new BN(40) });
+
+            assert((await masset.balanceOf(sa.default)).eq(new BN(40)));
+            assert((await masset.totalSupply()).eq(new BN(40)));
+
+            assert((await b1.balanceOf(masset.address)).eq(new BN(10)));
+            assert((await b2.balanceOf(masset.address)).eq(new BN(10)));
+            assert((await b3.balanceOf(masset.address)).eq(new BN(10)));
+            assert((await b4.balanceOf(masset.address)).eq(new BN(10)));
+
+            // Rewards updated
+            let data: any = await rewardsContract.getTrancheData(0);
+            //let totalMintVol,  = data[0];
+            //let [totalMintVolume, totalRewardUnits] = data
+            let rewardStartTime = await rewardsContract.rewardStartTime();
+            validateTrancheDates(data, rewardStartTime, 0);
+
+            assert(data.totalMintVolume.eq(new BN(40)));
+            assert(data.totalRewardUnits.eq(new BN(0)));
+            assert(data.unclaimedRewardUnits.eq(new BN(0)));
+
+            // Reward for the user
+            data = await rewardsContract.getRewardeesData(0, [sa.default]);
+            assert(data.mintVolume[0].eq(new BN(40)));
+            assert(data.claimed[0] === false);
+            assert(data.rewardAllocation[0].eq(new BN(0)));
+            assert(data.redeemed[0] === false);
+
+        });
+    });
+
+    describe("claimReward()", () => {
+
+        it("User should claim reward");
+    });
+
+    describe("claimReward() with rewardee", () => {
+
+        it("User should claim reward");
+    });
+
+    describe("redeemReward()", () => {
+        it("Should redeem reward");
+    });
+
+    describe("fundTranche()", () => {
+        context("when the Rewards contract deployed", () => {
+            context("Should fail", async () => {
+                it("when fundTranche() is called by non governer");
+            });
+        });
+        context("getTrancheData()", () => {
+            it("Should have Tranche data after funding");
+        });
     });
 });
+
+function validateTrancheDates(trancheData, rewardStartTime, trancheNumber) {
+    let calcDatesData = genTrancheDate(rewardStartTime, trancheNumber);
+    assert(trancheData[0].eq(calcDatesData[0]));
+    assert(trancheData[1].eq(calcDatesData[1]));
+    assert(trancheData[2].eq(calcDatesData[2]));
+    assert(trancheData[3].eq(calcDatesData[3]));
+}
+
+function genTrancheDate(rewardStartTime, trancheNumber) {
+    const WEEK = new BN(60 * 60 * 24 * 7); // 1 week
+    const TRANCHE_PERIOD = (new BN(4)).mul(WEEK); // 4 week
+    const CLAIM_PERIOD = (new BN(8)).mul(WEEK);   // 8 weeks
+    const LOCKUP_PERIOD = (new BN(52)).mul(WEEK); // 52 weeks
+    let trancheData = new Array();
+    // startTime
+    trancheData[0] = rewardStartTime.add((new BN(trancheNumber)).mul(TRANCHE_PERIOD));
+    // endTime
+    trancheData[1] = trancheData[0].add(TRANCHE_PERIOD);
+    // claimEndTime
+    trancheData[2] = trancheData[1].add(CLAIM_PERIOD);
+    // unlockTime
+    trancheData[3] = trancheData[1].add(LOCKUP_PERIOD);
+    return trancheData;
+}
+
