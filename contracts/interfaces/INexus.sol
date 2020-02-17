0cead202328df627cf1c31e21e43e736dae3f21e
alsco77 <alsco77@pm.me>
2020-02-17 16:35:15 -0700

    Updated interface

--- contracts/interfaces/INexus.sol
+++ contracts/interfaces/INexus.sol
@@ -9,9 +9,9 @@ interface INexus {
     function getModule(bytes32 key) external view returns (address);
 
     function proposeModule(bytes32 _key, address _addr) external;
-    function cancelProposedModule(bytes32 _key, address _addr) external;
-    function acceptProposedModule(bytes32 _key, address _addr) external;
-    function acceptProposedModules(bytes32[] calldata _keys, address[] calldata _addrs) external;
+    function cancelProposedModule(bytes32 _key) external;
+    function acceptProposedModule(bytes32 _key) external;
+    function acceptProposedModules(bytes32[] calldata _keys) external;
 
     function requestLockModule(bytes32 _key) external;
     function cancelLockModule(bytes32 _key) external;
