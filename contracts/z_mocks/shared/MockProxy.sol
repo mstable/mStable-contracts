ebf2c1836e8665e1648ba332c01b2ae1a9464ef3
alsco77 <alsco77@pm.me>
2020-03-16 13:12:02 +0100

    Added mock to generate typings

---
+++ contracts/z_mocks/shared/MockProxy.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.5.16;
+
+import { InitializableAdminUpgradeabilityProxy } from "@openzeppelin/upgrades/contracts/upgradeability/InitializableAdminUpgradeabilityProxy.sol";
+
+contract MockProxy is InitializableAdminUpgradeabilityProxy {
+  
+}
