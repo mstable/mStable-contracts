d6b6522055ac521737613a523e807aff1dfa80d7
Jitendra Chittoda <jcchittoda@gmail.com>
2020-03-27 15:29:07 +0530

    Basic test for CompoundIntegration working

---
+++ test/masset/platform-integrations/TestCompoundIntegration.spec.ts
@@ -0,0 +1,382 @@
+/* eslint-disable @typescript-eslint/camelcase */
+/* eslint-disable consistent-return */
+
+import * as t from "types/generated";
+import { constants, expectEvent, shouldFail } from "openzeppelin-test-helpers";
+import { BN } from "@utils/tools";
+import { StandardAccounts, SystemMachine, MassetMachine } from "@utils/machines";
+import { MainnetAccounts, ZERO_ADDRESS, MAX_UINT256, ZERO } from "@utils/constants";
+
+import envSetup from "@utils/env_setup";
+import {
+    BassetIntegrationDetails,
+    Platform,
+    CTokenDetails,
+    ATokenDetails,
+    Address,
+} from "../../../types";
+import shouldBehaveLikeModule from "../../shared/behaviours/Module.behaviour";
+
+const { expect, assert } = envSetup.configure();
+
+const c_MockERC20: t.MockERC20Contract = artifacts.require("MockERC20");
+const c_MockCToken: t.MockCTokenContract = artifacts.require("MockCToken");
+
+const c_Nexus: t.NexusContract = artifacts.require("Nexus");
+const c_DelayedProxyAdmin: t.DelayedProxyAdminContract = artifacts.require("DelayedProxyAdmin");
+
+const c_InitializableProxy: t.InitializableAdminUpgradeabilityProxyContract = artifacts.require(
+    "@openzeppelin/upgrades/InitializableAdminUpgradeabilityProxy",
+);
+const c_CompoundIntegration: t.CompoundIntegrationContract = artifacts.require(
+    "CompoundIntegration",
+);
+
+contract("CompoundIntegration", async (accounts) => {
+    const sa = new StandardAccounts(accounts);
+    const ma = new MainnetAccounts();
+
+    let systemMachine: SystemMachine;
+    let nexus: t.NexusInstance;
+    let massetMachine: MassetMachine;
+
+    let compoundPlatformAddress = ZERO_ADDRESS; // No platform specific address for Compound
+
+    let integrationDetails: BassetIntegrationDetails;
+    let d_DelayedProxyAdmin: t.DelayedProxyAdminInstance;
+    let d_CompoundIntegrationProxy: t.InitializableAdminUpgradeabilityProxyInstance;
+    let d_CompoundIntegration: t.CompoundIntegrationInstance;
+
+    const ctx: { module?: t.InitializableModuleInstance } = {};
+
+    before("base init", async () => {
+        systemMachine = new SystemMachine(sa.all);
+        massetMachine = systemMachine.massetMachine;
+
+        await runSetup();
+    });
+
+    const runSetup = async () => {
+        // SETUP
+        // ======
+        nexus = await c_Nexus.new(sa.governor);
+        // Init proxyAdmin
+        d_DelayedProxyAdmin = await c_DelayedProxyAdmin.new(nexus.address);
+        // Initialize the proxy
+        d_CompoundIntegrationProxy = await c_InitializableProxy.new();
+        d_CompoundIntegration = await c_CompoundIntegration.at(d_CompoundIntegrationProxy.address);
+
+        // Load network specific integration data
+        integrationDetails = await massetMachine.loadBassets();
+
+        // Initialize the proxy storage
+        const compoundImplementation = await c_CompoundIntegration.new();
+
+        const initializationData_CompoundIntegration: string = compoundImplementation.contract.methods
+            .initialize(
+                nexus.address,
+                [sa.default],
+                compoundPlatformAddress,
+                integrationDetails.cTokens.map((c) => c.bAsset),
+                integrationDetails.cTokens.map((c) => c.cToken),
+            )
+            .encodeABI();
+        await d_CompoundIntegrationProxy.initialize(
+            compoundImplementation.address,
+            d_DelayedProxyAdmin.address,
+            initializationData_CompoundIntegration,
+        );
+
+        await nexus.initialize(
+            [await d_DelayedProxyAdmin.Key_ProxyAdmin()],
+            [d_DelayedProxyAdmin.address],
+            [true],
+            sa.governor,
+            { from: sa.governor },
+        );
+
+        ctx.module = d_CompoundIntegration;
+    };
+
+    describe("initializing CompoundIntegration", async () => {
+        describe("verifying GovernableWhitelist initialization", async () => {
+            describe("verifying InitializableModule initialization", async () => {
+                shouldBehaveLikeModule(ctx as Required<typeof ctx>, sa);
+
+                it("should properly store valid arguments", async () => {
+                    // Check for nexus addr
+                    expect(await d_CompoundIntegration.nexus()).eq(nexus.address);
+                });
+            });
+
+            it("should properly store valid arguments", async () => {
+                // check for whitelisted accs
+                const whitelisted = await d_CompoundIntegration.whitelist(sa.default);
+                expect(whitelisted).eq(true);
+                // check for non whitelisted accs
+                const notWhitelisted = await d_CompoundIntegration.whitelist(sa.dummy4);
+                expect(notWhitelisted).eq(false);
+                const notWhitelisted2 = await d_CompoundIntegration.whitelist(sa.governor);
+                expect(notWhitelisted2).eq(false);
+            });
+            it("should fail when empty whitelisted array", async () => {
+                const tempImpl = await c_CompoundIntegration.new();
+                const erc20Mock = await c_MockERC20.new("TMP", "TMP", 18, sa.default, "1000000");
+                const aTokenMock = await c_MockCToken.new(erc20Mock.address);
+                await shouldFail.reverting.withMessage(
+                    tempImpl.initialize(
+                        nexus.address,
+                        [],
+                        sa.other,
+                        [erc20Mock.address],
+                        [aTokenMock.address],
+                    ),
+                    "Empty whitelist array",
+                );
+            });
+
+            it("should fail when whitelisted address is zero or duplicate", async () => {
+                const tempImpl = await c_CompoundIntegration.new();
+                const erc20Mock = await c_MockERC20.new("TMP", "TMP", 18, sa.default, "1000000");
+                const aTokenMock = await c_MockCToken.new(erc20Mock.address);
+                await shouldFail.reverting.withMessage(
+                    tempImpl.initialize(
+                        nexus.address,
+                        [sa.dummy1, sa.dummy1],
+                        compoundPlatformAddress,
+                        [erc20Mock.address],
+                        [aTokenMock.address],
+                    ),
+                    "Already whitelisted",
+                );
+                await shouldFail.reverting.withMessage(
+                    tempImpl.initialize(
+                        nexus.address,
+                        [ZERO_ADDRESS],
+                        compoundPlatformAddress,
+                        [erc20Mock.address],
+                        [aTokenMock.address],
+                    ),
+                    "Address is zero",
+                );
+            });
+        });
+
+        it("should properly store valid arguments", async () => {
+            // check for platform addr
+            expect(compoundPlatformAddress).eq(await d_CompoundIntegration.platformAddress());
+
+            // check for pTokens added & events
+            expect(integrationDetails.cTokens[0].cToken).eq(
+                await d_CompoundIntegration.bAssetToPToken(integrationDetails.cTokens[0].bAsset),
+            );
+
+            expect(integrationDetails.cTokens[1].cToken).eq(
+                await d_CompoundIntegration.bAssetToPToken(integrationDetails.cTokens[1].bAsset),
+            );
+        });
+
+        it("should approve spending of the passed bAssets", async () => {
+            // const bAsset = await c_MockERC20.at(integrationDetails.aTokens[0].bAsset);
+            // const addressProvider = await c_AaveLendingPoolAddressProvider.at(
+            //     integrationDetails.aavePlatformAddress,
+            // );
+            // const approvedAddress = await addressProvider.getLendingPoolCore();
+            // const balance = await bAsset.allowance(d_CompoundIntegration.address, approvedAddress);
+            // expect(balance).bignumber.eq(MAX_UINT256 as any);
+        });
+
+        it("should fail when called again", async () => {
+            const tempImpl = await c_CompoundIntegration.new();
+            const erc20Mock = await c_MockERC20.new("TMP", "TMP", 18, sa.default, "1000000");
+            const aTokenMock = await c_MockCToken.new(erc20Mock.address);
+            await tempImpl.initialize(
+                nexus.address,
+                [sa.dummy1],
+                compoundPlatformAddress,
+                [erc20Mock.address],
+                [aTokenMock.address],
+            );
+            await shouldFail.reverting.withMessage(
+                tempImpl.initialize(
+                    nexus.address,
+                    [sa.dummy1],
+                    compoundPlatformAddress,
+                    [erc20Mock.address],
+                    [aTokenMock.address],
+                ),
+                "Contract instance has already been initialized",
+            );
+        });
+
+        it("should fail if passed incorrect data", async () => {
+            const tempImpl = await c_CompoundIntegration.new();
+            const erc20Mock = await c_MockERC20.new("TMP", "TMP", 18, sa.default, "1000000");
+            const aTokenMock = await c_MockCToken.new(erc20Mock.address);
+
+            // bAsset and pToken array length are different
+            await shouldFail.reverting.withMessage(
+                tempImpl.initialize(
+                    nexus.address,
+                    [sa.dummy1, sa.dummy2],
+                    compoundPlatformAddress,
+                    [erc20Mock.address],
+                    [aTokenMock.address, aTokenMock.address],
+                ),
+                "Invalid input arrays",
+            );
+            // pToken address is zero
+            await shouldFail.reverting.withMessage(
+                tempImpl.initialize(
+                    nexus.address,
+                    [sa.dummy1, sa.dummy2],
+                    compoundPlatformAddress,
+                    [erc20Mock.address],
+                    [ZERO_ADDRESS],
+                ),
+                "Invalid addresses",
+            );
+            // duplicate pToken or bAsset
+            await shouldFail.reverting.withMessage(
+                tempImpl.initialize(
+                    nexus.address,
+                    [sa.dummy1, sa.dummy2],
+                    compoundPlatformAddress,
+                    [erc20Mock.address, erc20Mock.address],
+                    [aTokenMock.address, sa.default],
+                ),
+                "pToken already set",
+            );
+            // invalid bAsset addresses
+            await shouldFail.reverting(
+                tempImpl.initialize(
+                    nexus.address,
+                    [sa.dummy1, sa.dummy2],
+                    compoundPlatformAddress,
+                    [sa.default],
+                    [aTokenMock.address],
+                ),
+            );
+        });
+    });
+
+    describe("setting P Token Address", async () => {
+        let erc20Mock: t.MockERC20Instance;
+        let cTokenMock: t.MockCTokenInstance;
+        beforeEach("init mocks", async () => {
+            erc20Mock = await c_MockERC20.new("TMP", "TMP", 18, sa.default, "1000000");
+            cTokenMock = await c_MockCToken.new(erc20Mock.address);
+            await runSetup();
+        });
+
+        it("should pass only when function called by the Governor", async () => {
+            await shouldFail.reverting.withMessage(
+                d_CompoundIntegration.setPTokenAddress(erc20Mock.address, cTokenMock.address, {
+                    from: sa.default,
+                }),
+                "Only governor can execute",
+            );
+            await d_CompoundIntegration.setPTokenAddress(erc20Mock.address, cTokenMock.address, {
+                from: sa.governor,
+            });
+            expect(cTokenMock.address).eq(
+                await d_CompoundIntegration.bAssetToPToken(erc20Mock.address),
+            );
+        });
+
+        it("should approve the spending of the bAsset correctly and emit event", async () => {
+            await d_CompoundIntegration.setPTokenAddress(erc20Mock.address, cTokenMock.address, {
+                from: sa.governor,
+            });
+            expect(cTokenMock.address).eq(
+                await d_CompoundIntegration.bAssetToPToken(erc20Mock.address),
+            );
+
+            const approvedAddress = cTokenMock.address;
+            const balance = await erc20Mock.allowance(
+                d_CompoundIntegration.address,
+                approvedAddress,
+            );
+            expect(balance).bignumber.eq(MAX_UINT256 as any);
+        });
+        it("should fail when passed invalid args", async () => {
+            // bAsset address is zero
+            await shouldFail.reverting.withMessage(
+                d_CompoundIntegration.setPTokenAddress(ZERO_ADDRESS, cTokenMock.address, {
+                    from: sa.governor,
+                }),
+                "Invalid addresses",
+            );
+            // pToken address is zero
+            await shouldFail.reverting.withMessage(
+                d_CompoundIntegration.setPTokenAddress(erc20Mock.address, ZERO_ADDRESS, {
+                    from: sa.governor,
+                }),
+                "Invalid addresses",
+            );
+            // pToken address already assigned for a bAsset
+            await d_CompoundIntegration.setPTokenAddress(erc20Mock.address, cTokenMock.address, {
+                from: sa.governor,
+            });
+            await shouldFail.reverting.withMessage(
+                d_CompoundIntegration.setPTokenAddress(erc20Mock.address, sa.default, {
+                    from: sa.governor,
+                }),
+                "pToken already set",
+            );
+        });
+    });
+
+    describe("calling deposit", async () => {
+        it("should only allow a whitelisted user to call function");
+        it("should deposit tokens to Aave", async () => {
+            // check that the lending pool core has tokens
+            // check that our new balance of aTokens is given
+            // should give accurate return value
+        });
+
+        it("should deposit all if there is no fee");
+        it("should handle the fee calculations", async () => {
+            // should deduct the transfer fee from the return value
+        });
+
+        it("should fail if we do not first pass the required bAsset");
+        it("should fail with broken arguments");
+        it("should fail if the bAsset is not supported");
+    });
+
+    describe("withdraw", async () => {
+        it("should only allow a whitelisted user to call function");
+        it("should withdraw tokens from Aave", async () => {
+            // check that the recipient receives the tokens
+            // check that the lending pool core has tokens
+            // check that our new balance of aTokens is given
+            // should give accurate return value
+        });
+
+        it("should withdraw all if there is no fee");
+        it("should handle the fee calculations", async () => {
+            // should deduct the transfer fee from the return value
+        });
+
+        it("should fail if there is insufficient balance");
+        it("should fail with broken arguments");
+        it("should fail if the bAsset is not supported");
+    });
+
+    describe("checkBalance", async () => {
+        it("should return balance when supported token address passed");
+        it("should increase our balance over time and activity");
+        it("should return balance with same precision as bAsset");
+    });
+
+    describe("reApproveAllTokens", async () => {
+        it("should only be callable bby the Governor");
+
+        it("should be able to be called multiple times");
+    });
+
+    describe("disapprove", async () => {
+        it("should be implemented...");
+    });
+});
