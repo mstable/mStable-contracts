acd1df3331642c119a3182786394ed06d9284aaf
George Ornbo <george@shapeshed.com>
2020-09-17 15:10:03 +0100

    Inital addLiquidation implementation

--- contracts/liquidator/Liquidator.sol
+++ contracts/liquidator/Liquidator.sol
@@ -33,6 +33,18 @@ contract Liquidator is
     event LiquidationTriggered(address indexed bAsset);
     event LiquidationCollected(address indexed bAsset);
 
+    struct Liquidation {
+        address basset;
+        address integration;
+        address rewardToken;
+        uint    amount;
+        uint    collectDrip;
+        bool    paused;
+    }
+
+    // Module-key => Liquidation
+    mapping(address => Liquidation) public liquidations;
+
     /**
      * @dev Constructor
      * @notice To avoid variable shadowing appended `Arg` after arguments name.
@@ -46,5 +58,44 @@ contract Liquidator is
         InitializableModule._initialize(_nexus);
         InitializableReentrancyGuard._initialize();
     }
+
+    //function addLiquidation(address _bAsset, address _integration, uint _amount ) external;
+    /**
+    * @dev Propose a new or update existing module
+    * @param _bAsset The _bAsset 
+    * @param _integration The integration contract address for the _bAsset
+    * @param _amount The amount to be sold
+    */
+    function addLiquidation(
+        address _bAsset, 
+        address _integration, 
+        uint _amount
+    )
+        external
+        onlyGovernor
+    {
+        require(_bAsset != address(0), "_bAsset cannot be zero address");
+        require(_integration != address(0), "integration cannot be zero address");
+
+        Liquidation storage liq = liquidations[_bAsset];
+
+        liq.basset = _bAsset;
+        liq.integration = _integration;
+        emit LiquidationAdded(_bAsset);
+    }
+
+
+    //function getLiquidation(address _bAsset) external view returns (Liquidation memory liquidation);
+
+    // Restricted to the `integrationContract` given in addLiquidation
+    //function collect() external;
+
+    // Callable by anyone to trigger a selling event 
+    //function triggerLiquidation(address _bAsset) external;
+
+    // Governor only
+    //function removeLiquidation(address _bAsset) external;
+    //function pauseLiquidation(address _bAsset) external;
+    //function setCollectDrip(address _bAsset) external;
 
 }
--- test/liquidator/TestLiquidatorContract.spec.ts
+++ test/liquidator/TestLiquidatorContract.spec.ts
@@ -5,6 +5,7 @@ import { BN } from "@utils/tools";
 import envSetup from "@utils/env_setup";
 import * as t from "types/generated";
 import shouldBehaveLikeModule from "../shared/behaviours/Module.behaviour";
+import { ZERO_ADDRESS } from "@utils/constants";
 
 const Liquidator = artifacts.require("Liquidator");
 const MockNexus = artifacts.require("MockNexus");
@@ -42,6 +43,39 @@ contract("Liquidator", async (accounts) => {
 
         it("should properly store valid arguments", async () => {
             expect(await liquidator.nexus()).eq(systemMachine.nexus.address);
+        });
+    });
+
+    describe("addLiquidation()", () => {
+        it("should revert if not called by the Governor", async () => {
+            await expectRevert(
+                liquidator.addLiquidation(ZERO_ADDRESS, ZERO_ADDRESS, new BN(18), {
+                    from: sa.default,
+                }),
+                "Only governor can execute",
+            );
+        });
+        it("should revert if passed a zero address for _bAsset", async () => {
+            await expectRevert(
+                liquidator.addLiquidation(ZERO_ADDRESS, ZERO_ADDRESS, new BN(18), {
+                    from: sa.governor,
+                }),
+                "bAsset cannot be zero address",
+            );
+        });
+        it("should revert if passed a zero address for _integration", async () => {
+            await expectRevert(
+                liquidator.addLiquidation(sa.dummy1, ZERO_ADDRESS, new BN(1), {
+                    from: sa.governor,
+                }),
+                "integration cannot be zero address",
+            );
+        });
+        it("should emit an event after adding a liquidation", async () => {
+            const tx = await liquidator.addLiquidation(sa.dummy1, sa.dummy2, new BN(1), {
+                from: sa.governor,
+            });
+            await expectEvent(tx.receipt, "LiquidationAdded");
         });
     });
 });
