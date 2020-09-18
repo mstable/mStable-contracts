b7bbdcb3c96a33e5c3be98702508223086d41088
George Ornbo <george@shapeshed.com>
2020-09-18 10:46:03 +0100

    Add Uniswap mock

---
+++ contracts/z_mocks/integrations/Uniswap.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.5.16;
+
+contract MockUniswap {
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts) {
+        //
+        
+    };
+}
