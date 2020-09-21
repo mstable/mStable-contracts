0e86d0bdcdfd59ed69b28c42f496d13c6d8e2667
George Ornbo <george@shapeshed.com>
2020-09-21 12:31:13 +0100

    Add missing interface

---
+++ contracts/masset/platform-integrations/IUniswapV2Router02.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.16;
+
+contract IUniswapV2Router02 {
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external returns (uint[] memory amounts);
+}
