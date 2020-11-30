d4ddccae9f7260942a543dad3c2064a3b907bb7e
lovrobiljeskovic <biljeskovic.lovro@gmail.com>
2020-11-30 09:47:55 +0100

    Pushed mock examples for `SaveViaUniswap` & `SaveViaMint`

--- test/savings/save-with-anything/TestSaveViaUniswap.spec.ts
+++ test/savings/save-with-anything/TestSaveViaUniswap.spec.ts
@@ -6,8 +6,8 @@ import * as t from "types/generated";
 const MockERC20 = artifacts.require("MockERC20");
 const SavingsManager = artifacts.require("SavingsManager");
 const MockNexus = artifacts.require("MockNexus");
-const MockMasset = artifacts.require("MockMasset");
-const SaveViaMint = artifacts.require("SaveViaMint");
+const SaveViaUniswap = artifacts.require("SaveViaUniswap");
+const MockUniswap = artifacts.require("MockUniswap");
 
 contract("SavingsContract", async (accounts) => {
     const sa = new StandardAccounts(accounts);
@@ -15,17 +15,19 @@ contract("SavingsContract", async (accounts) => {
     let bAsset: t.MockERC20Instance;
     let mUSD: t.MockERC20Instance;
     let savings: t.SavingsManagerInstance;
-    let saveViaMint: t.SaveViaMint;
+    let saveViaUniswap: t.SaveViaUniswap;
     let nexus: t.MockNexusInstance;
+    let uniswap: t.MockUniswap;
 
     const setupEnvironment = async (): Promise<void> => {
         // deploy contracts
         bAsset = await MockERC20.new("Mock coin", "MCK", 18, sa.fundManager, 100000000);
         mUSD = await MockERC20.new("mStable USD", "mUSD", 18, sa.fundManager, 100000000);
+        uniswap = await MockUniswap.new();
         savings = await SavingsManager.new(nexus.address, mUSD.address, sa.other, {
             from: sa.default,
         });
-        saveViaMint = SaveViaMint.new(savings.address);
+        saveViaUniswap = await SaveViaUniswap.new(savings.address, uniswap.address);
     };
 
     before(async () => {
@@ -33,9 +35,9 @@ contract("SavingsContract", async (accounts) => {
         await setupEnvironment();
     });
 
-    describe("saving via mint", async () => {
-        it("should mint tokens & deposit", async () => {
-            saveViaMint.mintAndSave(mUSD.address, bAsset, 100); // how to get all the params here?
+    describe("saving via uniswap", async () => {
+        it("should swap tokens & deposit", async () => {
+            saveViaUniswap.buyAndSave(); // how to get all the params here?
         });
     });
 });
