f2ea0601e5961f178f998552ee65c51eaef2cfd4
lovrobiljeskovic <biljeskovic.lovro@gmail.com>
2020-12-03 16:38:53 +0100

    Added `approve` to constructors Removed uncecessary variables Updated `buyAndSave` fn in `SaveViaUniswap` with curve contract call Extended `IUniswapRouter02` with `getAmountsOut`

--- contracts/savings/save-with-anything/SaveViaMint.sol
+++ contracts/savings/save-with-anything/SaveViaMint.sol
@@ -1,20 +1,20 @@
 pragma solidity 0.5.16;
 
-import { ISavingsContract } from "../../interfaces/ISavingsContract.sol";
+import { IMasset } from "../../interfaces/IMasset.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ISavingsContract } from "../../interfaces/ISavingsContract.sol";
 
 contract SaveViaMint {
 
     address save;
-    address mAsset = "";
 
-    constructor(address _save) public {
+    constructor(address _save, address _bAsset, uint _bAssetAmount) public {
         save = _save;
+        IERC20(_bAsset).approve(save, _bAssetAmount);
     }
 
     function mintAndSave(address _mAsset, address _bAsset, uint _bassetAmount) external {
-        IERC20(_bAsset).transferFrom(msg.sender, address(this), _bassetAmount);
-        IERC20(_bAsset).approve(address(this), _bassetAmount);
+        IERC20(_bAsset).transferFrom(msg.sender, save, _bassetAmount);
         IMasset mAsset = IMasset(_mAsset);
         uint massetsMinted = mAsset.mint(_bAsset, _bassetAmount);
         ISavingsContract(save).deposit(massetsMinted, msg.sender);
--- contracts/savings/save-with-anything/SaveViaUniswap.sol
+++ contracts/savings/save-with-anything/SaveViaUniswap.sol
@@ -1,44 +1,62 @@
 pragma solidity 0.5.16;
 
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import { ISavingsContract } from "../../interfaces/ISavingsContract.sol";
 import { IUniswapV2Router02 } from "../../masset/liquidator/IUniswapV2Router02.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { ICurveMetaPool } from "../../masset/liquidator/ICurveMetaPool.sol";
+import { IBasicToken } from "../../shared/IBasicToken.sol";
 
 contract SaveViaUniswap {
 
+    using SafeERC20 for IERC20;
+    using SafeMath for uint;
     address save;
-    address mAsset = "";
+    ICurveMetaPool curve;
     IUniswapV2Router02 uniswap;
 
-    constructor(address _save, address _uniswapAddress) public {
+    constructor(address _save, address _uniswapAddress, address _curveAddress, address _bAsset, uint _bAssetAmount) public {
+        require(_save != address(0), "Invalid save address");
         save = _save;
+        require(_uniswapAddress != address(0), "Invalid uniswap address");
         uniswap = IUniswapV2Router02(_uniswapAddress);
+        require(_curveAddress != address(0), "Invalid curve address");
+        curve = ICurveMetaPool(_curveAddress);
+        IERC20(_bAsset).safeApprove(address(uniswap), _bAssetAmount);
+        IERC20(_bAsset).safeApprove(address(uniswap), _bAssetAmount);
+        IERC20(_bAsset).safeApprove(address(curve), _bAssetAmount);
     }
-    // 1. Approve this contract to spend the sell token (e.g. ETH)
-    // 2. calculate the _path and other data relevant to the purchase off-chain
-    // 3. Calculate the "min buy amount" if any, off chain
-    function buyAndSave(address token, uint amountIn, uint amountOutMin, address[] calldata path, uint deadline) external {
-        IERC20(token).transferFrom(msg.sender, address(this), amountIn);
-        IERC20(token).approve(address(uniswap), amountIn);
-        uint[] amounts = uniswap.swapExactTokensForTokens(
-        amountIn,
-        amountOutMin, //how do I get this value exactly?
-        getPath(token),
-        address(this),
-        deadline
+
+    function buyAndSave (
+        address _bAsset,
+        uint _bAssetAmount,
+        uint _amountOutMin,
+        address[] calldata _path,
+        uint _deadline,
+        int128 _curvePosition
+        ) external {
+        IERC20(_bAsset).transferFrom(msg.sender, address(this), _bAssetAmount);
+        uint[] memory amounts = uniswap.swapExactTokensForTokens(
+            _bAssetAmount,
+            _amountOutMin,
+            _path,
+            address(save),
+            _deadline
         );
-        ISavingsContract(save).deposit(amounts[1], msg.sender);
+        // I copied this from the Liquidator contract, I am unsure about the second and last parameter in crv fn)
+        uint256 bAssetDec = IBasicToken(_bAsset).decimals();
+        uint256 minOutCrv = _bAssetAmount.mul(95e16).div(10 ** bAssetDec);
+        uint purchased = curve.exchange_underlying(_curvePosition, 0, amounts[1], minOutCrv);
+        ISavingsContract(save).deposit(purchased, msg.sender);
     }
 
-    function getPath(address token) private view returns (address[] memory) {
-        address[] memory path = new address[](3);
-        path[0] = token;
-        path[1] = uniswap.ETH();
-        path[2] = mAsset;
-        return path;
+    // when you say off-chain does it mean we compute the values on the FE?
+    function getAmountsOutForTokenValue(uint _bAssetAmount, address[] memory _path) public view returns (uint[] memory) {
+        return uniswap.getAmountsOut(_bAssetAmount, _path);
     }
 
-    function getEstimatedAmountForToken(address token, uint tokenAmount) public view returns (uint[] memory) {
-        return uniswap.getAmountsIn(tokenAmount, getPath(token));
+    function getEstimatedAmountForToken(uint _tokenAmount, address[] memory _path) public view returns (uint[] memory) {
+        return uniswap.getAmountsIn(_tokenAmount, _path);
     }
 }
