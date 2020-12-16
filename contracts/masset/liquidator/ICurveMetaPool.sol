8ba1ce31c64e9d511801e12c6a12ea66d39fd80c
alsco77 <alsco77@pm.me>
2020-12-16 13:02:54 +0100

    Consolidated the save wrapper fns

--- contracts/masset/liquidator/ICurveMetaPool.sol
+++ contracts/masset/liquidator/ICurveMetaPool.sol
@@ -2,4 +2,5 @@ pragma solidity 0.5.16;
 
 interface ICurveMetaPool {
     function exchange_underlying(int128 i, int128 j, uint256 dx, uint256 min_dy) external returns (uint256);
+    function get_dy(int128 i, int128 j, uint256 dx) external view returns (uint256);
 }
