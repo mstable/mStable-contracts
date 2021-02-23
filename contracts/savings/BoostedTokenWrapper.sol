101ae77c2c7a0399ddb96b60117a71c82a9e80f3
alsco77 <alsco77@pm.me>
2021-02-23 13:06:19 +0000

    Remove erroneous files and apply linter

--- contracts/savings/BoostedTokenWrapper.sol
+++ contracts/savings/BoostedTokenWrapper.sol
@@ -5,10 +5,7 @@ pragma solidity 0.8.0;
 import { IIncentivisedVotingLockup } from "../interfaces/IIncentivisedVotingLockup.sol";
 
 // Libs
-import {
-    SafeERC20,
-    IERC20
-} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import { InitializableReentrancyGuard } from "../shared/InitializableReentrancyGuard.sol";
 import { StableMath } from "../shared/StableMath.sol";
 import { Root } from "../shared/Root.sol";
