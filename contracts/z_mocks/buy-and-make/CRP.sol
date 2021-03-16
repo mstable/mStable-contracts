f03f86ebc77c2c96c4009ec02c4e22d34778c3de
alsco77 <alsco77@pm.me>
2021-03-16 19:30:47 +0000

    Add deploy script

---
+++ contracts/z_mocks/buy-and-make/CRP.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.0;
+pragma abicoder v2;
+
+abstract contract ConfigurableRightsPool  {
+
+    function createPool(uint initialSupply)
+        external
+        virtual;
+
+    function whitelistLiquidityProvider(address provider)
+        external
+        virtual;
+    
+    function setController(address newOwner) external virtual;
+}
