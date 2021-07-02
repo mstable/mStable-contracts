c840b8fdf66c7013c0f5dbcfd8aa7072303f8561
chrisjgf <chrisjgf@gmail.com>
2021-07-02 12:30:08 +0100

    Prettier config

--- contracts/rewards/RewardsDistributor.sol
+++ contracts/rewards/RewardsDistributor.sol
@@ -1,8 +1,9 @@
 pragma solidity 0.5.16;
 
 import { IRewardsDistributionRecipient } from "../interfaces/IRewardsDistributionRecipient.sol";
-
-import { InitializableGovernableWhitelist } from "../governance/InitializableGovernableWhitelist.sol";
+import {
+    InitializableGovernableWhitelist
+} from "../governance/InitializableGovernableWhitelist.sol";
 import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
 /**
@@ -12,19 +13,20 @@ import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20
  * to specified Reward Recipients.
  */
 contract RewardsDistributor is InitializableGovernableWhitelist {
-
     using SafeERC20 for IERC20;
 
     event RemovedFundManager(address indexed _address);
-    event DistributedReward(address funder, address recipient, address rewardToken, uint256 amount, address platformToken, uint256 platformAmount);
+    event DistributedReward(
+        address funder,
+        address recipient,
+        address rewardToken,
+        uint256 amount,
+        address platformToken,
+        uint256 platformAmount
+    );
 
     /** @dev Recipient is a module, governed by mStable governance */
-    constructor(
-        address _nexus,
-        address[] memory _fundManagers
-    )
-        public
-    {
+    constructor(address _nexus, address[] memory _fundManagers) public {
         InitializableGovernableWhitelist._initialize(_nexus, _fundManagers);
     }
 
@@ -32,10 +34,7 @@ contract RewardsDistributor is InitializableGovernableWhitelist {
      * @dev Allows the mStable governance to add a new FundManager
      * @param _address  FundManager to add
      */
-    function addFundManager(address _address)
-        external
-        onlyGovernor
-    {
+    function addFundManager(address _address) external onlyGovernor {
         _addWhitelist(_address);
     }
 
@@ -43,10 +42,7 @@ contract RewardsDistributor is InitializableGovernableWhitelist {
      * @dev Allows the mStable governance to remove inactive FundManagers
      * @param _address  FundManager to remove
      */
-    function removeFundManager(address _address)
-        external
-        onlyGovernor
-    {
+    function removeFundManager(address _address) external onlyGovernor {
         require(_address != address(0), "Address is zero");
         require(whitelist[_address], "Address is not whitelisted");
 
@@ -66,16 +62,13 @@ contract RewardsDistributor is InitializableGovernableWhitelist {
         IRewardsDistributionRecipient[] calldata _recipients,
         uint256[] calldata _amounts,
         uint256[] calldata _platformAmounts
-    )
-        external
-        onlyWhitelisted
-    {
+    ) external onlyWhitelisted {
         uint256 len = _recipients.length;
         require(len > 0, "Must choose recipients");
         require(len == _amounts.length, "Mismatching inputs");
         require(len == _platformAmounts.length, "Mismatching inputs");
 
-        for(uint i = 0; i < len; i++){
+        for (uint256 i = 0; i < len; i++) {
             uint256 amount = _amounts[i];
             uint256 platformAmount = _platformAmounts[i];
             IRewardsDistributionRecipient recipient = _recipients[i];
@@ -88,8 +81,14 @@ contract RewardsDistributor is InitializableGovernableWhitelist {
             // Only after successful tx - notify the contract of the new funds
             recipient.notifyRewardAmount(amount);
 
-            emit DistributedReward(msg.sender, address(recipient), address(rewardToken), amount, address(platformToken), platformAmount);
+            emit DistributedReward(
+                msg.sender,
+                address(recipient),
+                address(rewardToken),
+                amount,
+                address(platformToken),
+                platformAmount
+            );
         }
     }
-    
 }
