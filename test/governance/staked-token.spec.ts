c8170e15b3f57a2f191882f68104d49dd98dc286
Nicholas Addison <nick@addisonbrown.com.au>
2021-08-03 23:16:41 +1000

    chore: wip staked tests

--- test/governance/staked-token.spec.ts
+++ test/governance/staked-token.spec.ts
@@ -1,22 +1,155 @@
 import { ethers } from "hardhat"
-import { MassetMachine } from "@utils/machines"
-import { shouldBehaveLikeModule, IModuleBehaviourContext } from "../shared/Module.behaviour"
+import { MassetMachine, StandardAccounts } from "@utils/machines"
+import { MockNexus__factory } from "types/generated/factories/MockNexus__factory"
+import { AssetProxy__factory, MockERC20, MockERC20__factory, MockNexus } from "types"
+import { StakedToken } from "types/generated/StakedToken"
+import { StakedToken__factory } from "types/generated/factories/StakedToken__factory"
+import { DEAD_ADDRESS } from "index"
+import { ONE_DAY, ONE_WEEK } from "@utils/constants"
+import { BN, simpleToExactAmount } from "@utils/math"
+import { expect } from "chai"
+import { increaseTime } from "@utils/time"
+
+interface UserStakingData {
+    stakedBalance: BN
+    votes: BN
+    earnedRewards: BN
+    stakersCooldown: BN
+    rewardsBalance: BN
+}
 
 describe("Staked Token", () => {
-    const ctx: Partial<IModuleBehaviourContext> = {}
+    // const ctx: Partial<IModuleBehaviourContext> = {}
+    let sa: StandardAccounts
 
-    beforeEach("Create Contract", async () => {
+    let nexus: MockNexus
+    let rewardToken: MockERC20
+    let stakedToken: StakedToken
+
+    const redeployStakedToken = async (): Promise<StakedToken> => {
+        nexus = await new MockNexus__factory(sa.default.signer).deploy(sa.governor.address, DEAD_ADDRESS, DEAD_ADDRESS)
+        rewardToken = await new MockERC20__factory(sa.default.signer).deploy("Reward", "RWD", 18, sa.default.address, 10000000)
+
+        const stakedTokenFactory = await new StakedToken__factory(sa.default.signer)
+        const stakedTokenImpl = await stakedTokenFactory.deploy(
+            sa.default.address,
+            nexus.address,
+            rewardToken.address,
+            ONE_WEEK,
+            rewardToken.address,
+            ONE_WEEK,
+            ONE_DAY.mul(2),
+            ONE_WEEK.mul(4),
+        )
+        const rewardsDistributorAddress = DEAD_ADDRESS
+        const data = stakedTokenImpl.interface.encodeFunctionData("initialize", ["Staked Rewards", "stkRWD", rewardsDistributorAddress])
+        const stakedTokenProxy = await new AssetProxy__factory(sa.default.signer).deploy(stakedTokenImpl.address, DEAD_ADDRESS, data)
+
+        return stakedTokenFactory.attach(stakedTokenProxy.address)
+    }
+
+    const snapshotUserStakingData = async (user = sa.default): Promise<UserStakingData> => {
+        const stakedBalance = await stakedToken.balanceOf(user.address)
+        const votes = await stakedToken.getVotes(user.address)
+        const earnedRewards = await stakedToken.earned(user.address)
+        const stakersCooldown = await stakedToken.stakersCooldowns(user.address)
+        const rewardsBalance = await rewardToken.balanceOf(user.address)
+
+        return {
+            stakedBalance,
+            votes,
+            earnedRewards,
+            stakersCooldown,
+            rewardsBalance,
+        }
+    }
+
+    before("Create Contract", async () => {
         const accounts = await ethers.getSigners()
         const mAssetMachine = await new MassetMachine().initAccounts(accounts)
-        ctx.sa = mAssetMachine.sa
+        sa = mAssetMachine.sa
     })
 
     // shouldBehaveLikeModule(ctx as Required<typeof ctx>)
 
+    context("deploy and initialize", () => {
+        before(async () => {
+            stakedToken = await redeployStakedToken()
+        })
+        it("post initialize", async () => {
+            expect(await stakedToken.name()).to.eq("Staked Rewards")
+            expect(await stakedToken.symbol()).to.eq("stkRWD")
+            expect(await stakedToken.decimals()).to.eq(18)
+            expect(await stakedToken.rewardsDistributor()).to.eq(DEAD_ADDRESS)
+        })
+    })
+
     context("staking and delegating", () => {
-        it("should delegate to self by default")
-        it("should allow immediate delegating to a user")
-        it("should not allow token transfers")
+        const stakedAmount = simpleToExactAmount(1000)
+        beforeEach(async () => {
+            stakedToken = await redeployStakedToken()
+            await rewardToken.connect(sa.default.signer).approve(stakedToken.address, stakedAmount.mul(3))
+        })
+        it("should delegate to self by default", async () => {
+            expect(await stakedToken.balanceOf(sa.default.address), "staker stkRWD before").to.eq(0)
+            expect(await stakedToken.getVotes(sa.default.address), "staker votes before").to.eq(0)
+            expect(await stakedToken.totalSupply(), "total staked before").to.eq(0)
+
+            const tx = await stakedToken["stake(uint256,address)"](stakedAmount, sa.default.address)
+            await expect(tx).to.emit(stakedToken, "Staked").withArgs(sa.default.address, sa.default.address, stakedAmount)
+            await expect(tx).to.emit(stakedToken, "DelegateChanged").not
+            await expect(tx).to.emit(stakedToken, "DelegateVotesChanged").withArgs(sa.default.address, 0, stakedAmount)
+            await expect(tx).to.emit(rewardToken, "Transfer").withArgs(sa.default.address, stakedToken.address, stakedAmount)
+
+            expect(await stakedToken.balanceOf(sa.default.address), "staker stkRWD after").to.eq(stakedAmount)
+            expect(await stakedToken.getVotes(sa.default.address), "staker votes after").to.eq(stakedAmount)
+            expect(await stakedToken.totalSupply(), "total staked after").to.eq(stakedAmount)
+        })
+        it("should assign beneficiary to a different user but delegate is self", async () => {
+            expect(await stakedToken.balanceOf(sa.default.address), "staker stkRWD before").to.eq(0)
+            expect(await stakedToken.getVotes(sa.default.address), "staker votes before").to.eq(0)
+            expect(await stakedToken.balanceOf(sa.dummy1.address)).to.eq(0)
+            expect(await stakedToken.balanceOf(sa.dummy1.address), "beneficiary stkRWD before").to.eq(0)
+            expect(await stakedToken.getVotes(sa.dummy1.address), "beneficiary votes before").to.eq(0)
+
+            const tx = await stakedToken["stake(uint256,address)"](stakedAmount, sa.dummy1.address)
+            await expect(tx).to.emit(stakedToken, "Staked").withArgs(sa.default.address, sa.dummy1.address, stakedAmount)
+            await expect(tx).to.emit(stakedToken, "DelegateChanged").not
+            await expect(tx).to.emit(stakedToken, "DelegateVotesChanged").withArgs(sa.dummy1.address, 0, stakedAmount)
+            await expect(tx).to.emit(rewardToken, "Transfer").withArgs(sa.default.address, stakedToken.address, stakedAmount)
+
+            expect(await stakedToken.balanceOf(sa.default.address), "staker stkRWD after").to.eq(0)
+            expect(await stakedToken.getVotes(sa.default.address), "staker votes after").to.eq(0)
+            expect(await stakedToken.balanceOf(sa.dummy1.address), "beneficiary stkRWD after").to.eq(stakedAmount)
+            expect(await stakedToken.getVotes(sa.dummy1.address), "beneficiary votes after").to.eq(stakedAmount)
+            expect(await stakedToken.totalSupply(), "total staked after").to.eq(stakedAmount)
+        })
+        it("should assign beneficiary to a user and delegate a different user", async () => {
+            expect(await stakedToken.balanceOf(sa.default.address), "staker stkRWD before").to.eq(0)
+            expect(await stakedToken.getVotes(sa.default.address), "staker votes before").to.eq(0)
+            expect(await stakedToken.balanceOf(sa.dummy2.address), "beneficiary stkRWD before").to.eq(0)
+            expect(await stakedToken.getVotes(sa.dummy2.address), "beneficiary votes before").to.eq(0)
+            expect(await stakedToken.balanceOf(sa.dummy3.address), "delegatee stkRWD before").to.eq(0)
+            expect(await stakedToken.getVotes(sa.dummy3.address), "delegatee votes before").to.eq(0)
+
+            const tx = await stakedToken["stake(uint256,address,address)"](stakedAmount, sa.dummy2.address, sa.dummy3.address)
+            await expect(tx).to.emit(stakedToken, "Staked").withArgs(sa.default.address, sa.dummy2.address, stakedAmount)
+            await expect(tx).to.emit(stakedToken, "DelegateChanged").withArgs(sa.default.address, sa.default.address, sa.dummy3.address)
+            await expect(tx).to.emit(stakedToken, "DelegateVotesChanged").withArgs(sa.dummy3.address, 0, stakedAmount)
+            await expect(tx).to.emit(rewardToken, "Transfer").withArgs(sa.default.address, stakedToken.address, stakedAmount)
+
+            expect(await stakedToken.balanceOf(sa.default.address), "staker stkRWD after").to.eq(0)
+            expect(await stakedToken.getVotes(sa.default.address), "staker votes after").to.eq(0)
+            expect(await stakedToken.balanceOf(sa.dummy2.address), "beneficiary stkRWD after").to.eq(stakedAmount)
+            expect(await stakedToken.getVotes(sa.dummy2.address), "beneficiary votes after").to.eq(0)
+            expect(await stakedToken.balanceOf(sa.dummy3.address), "delegatee stkRWD after").to.eq(0)
+            expect(await stakedToken.getVotes(sa.dummy3.address), "delegatee votes after").to.eq(stakedAmount)
+        })
+    })
+
+    // context("change delegate votes")
+
+    context("withdraw staked tokens", () => {
         it("should extend the cooldown timer proportionately")
     })
 
@@ -49,8 +182,34 @@ describe("Staked Token", () => {
         it("should not cause a ridiculous amount of extra gas to trigger")
     })
 
-    context("unstaking", () => {
-        it("should not be possible before unstake window")
+    context("withdraw", () => {
+        beforeEach(async () => {
+            stakedToken = await redeployStakedToken()
+            const stakedAmount = simpleToExactAmount(2000)
+            await rewardToken.connect(sa.default.signer).approve(stakedToken.address, stakedAmount)
+            await stakedToken["stake(uint256,address)"](stakedAmount, sa.default.address)
+        })
+        const withdrawAmount = simpleToExactAmount(100)
+        it("should not be possible before cooldown started", async () => {
+            await expect(stakedToken.withdraw(withdrawAmount, sa.default.address, false)).to.revertedWith("UNSTAKE_WINDOW_FINISHED")
+        })
+        it("should not be possible before cooldown finished", async () => {
+            await stakedToken.startCooldown()
+            await increaseTime(ONE_DAY.mul(7).sub(60))
+            await expect(stakedToken.withdraw(withdrawAmount, sa.default.address, false)).to.revertedWith("INSUFFICIENT_COOLDOWN")
+        })
+        it("should after cooldown and in unstake window", async () => {
+            const beforeData = await snapshotUserStakingData(sa.default)
+
+            await stakedToken.startCooldown()
+            await increaseTime(ONE_DAY.mul(7).add(60))
+            await stakedToken.withdraw(withdrawAmount, sa.default.address, false)
+
+            const afterData = await snapshotUserStakingData(sa.default)
+            // expect(afterData.stakedBalance).to.eq(beforeData.stakedBalance.sub(withdrawAmount))
+            // expect(afterData.votes).to.eq(beforeData.votes.sub(withdrawAmount))
+            expect(afterData.rewardsBalance).to.eq(beforeData.rewardsBalance.add(withdrawAmount))
+        })
         it("should not be possible after the unstake window")
         it("should not reset the cooldown timer unless all is unstaked")
         it("should apply a redemption fee which is added to the pendingRewards from the rewards contract")
