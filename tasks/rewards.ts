945f5b2f83400df8a618a25433869bc72ed57257
Nicholas Addison <nick@addisonbrown.com.au>
2021-08-11 00:04:33 +1000

    chore: week 62 MTA rewards for Bal on Polygon

--- tasks/rewards.ts
+++ tasks/rewards.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-restricted-syntax */
 import { BN, simpleToExactAmount } from "@utils/math"
 import { task, types } from "hardhat/config"
-import rewardsFiles from "./balancer-mta-rewards/20210803.json"
+import rewardsFiles from "./balancer-mta-rewards/20210810.json"
 import { usdFormatter } from "./utils"
 
 task("sum-rewards", "Totals the rewards in a disperse json file")
