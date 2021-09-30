c21e2147fb0b2df99b74711412aa502a27bd213b
Nicholas Addison <nick@addisonbrown.com.au>
2021-09-30 21:44:18 +1000

    feat: added check that user has completed quest to quest-complete-queue

--- tasks/utils/quest-utils.ts
+++ tasks/utils/quest-utils.ts
@@ -1,5 +1,8 @@
+import axios from "axios"
 import { BigNumberish, Signer } from "ethers"
 import { arrayify, solidityKeccak256 } from "ethers/lib/utils"
+
+const questBookUrl = "https://europe-west1-mstable-questbook.cloudfunctions.net/questbook"
 
 export const signUserQuests = async (user: string, questIds: BigNumberish[], questSigner: Signer): Promise<string> => {
     const messageHash = solidityKeccak256(["address", "uint256[]"], [user, questIds])
@@ -11,4 +14,41 @@ export const signQuestUsers = async (questId: BigNumberish, users: string[], que
     const messageHash = solidityKeccak256(["uint256", "address[]"], [questId, users])
     const signature = await questSigner.signMessage(arrayify(messageHash))
     return signature
+}
+
+export const getQueuedUsersForQuest = async (questId: number): Promise<string[]> => {
+    // get users who have completed quests from the queue
+    const response = await axios.post(questBookUrl, {
+        query: `query { queue { userId ethereumId } }`,
+    })
+    const { queue } = response?.data?.data
+    if (!queue) {
+        console.log(response?.data)
+        throw Error(`Failed to get quests from queue`)
+    }
+    // filter users to just the migration quest
+    const usersInQueue = queue.filter((quest) => quest.ethereumId === questId)
+    const usersForQuest = usersInQueue.map((quest) => quest.userId)
+
+    return usersForQuest
+}
+
+export const hasUserCompletedQuest = async (user: string, questName: string): Promise<boolean> => {
+    const response = await axios.post(questBookUrl, {
+        query: `query { user(userId: "${user}") { quests {
+            id
+            complete
+            progress
+          } } }`,
+    })
+    const quests = response?.data?.data?.user?.quests
+    if (!quests) {
+        console.log(response?.data)
+        throw Error(`Failed to get quests for user ${user}`)
+    }
+
+    // Filter user's quest for the named quest. eg theGreatMigration
+    const quest = quests.find((q) => q.id === `${questName}.${user}`)
+
+    return quest?.complete
 }
