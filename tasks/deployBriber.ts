d0dec9ff29dd7690a452afbd752b7d2b50b52b7f
alsco77 <alsco77@pm.me>
2021-11-01 11:47:45 +0000

    chore: Add briber forwarding autotask

--- tasks/deployBriber.ts
+++ tasks/deployBriber.ts
@@ -1,7 +1,7 @@
 import "ts-node/register"
 import "tsconfig-paths/register"
 import { task, types } from "hardhat/config"
-import { GaugeBriber__factory } from "types/generated"
+import { GaugeBriber__factory, ERC20__factory, SavingsManager__factory } from "types/generated"
 import { deployContract } from "./utils/deploy-utils"
 import { getSigner } from "./utils/signerFactory"
 import { verifyEtherscan } from "./utils/etherscan"
@@ -31,6 +31,37 @@ task("deploy-GaugeBriber")
             address: gaugeBriber.address,
             contract: "contracts/buy-and-make/GaugeBriber.sol:GaugeBriber",
         })
+    })
+
+task("briber-forward")
+    .addOptionalParam("speed", "Defender Relayer speed param: 'safeLow' | 'average' | 'fast' | 'fastest'", "fast", types.string)
+    .setAction(async (taskArgs, hre) => {
+        const signer = await getSigner(hre, taskArgs.speed)
+        const chain = getChain(hre)
+
+        // LOAD
+        const savingsManagerAddr = resolveAddress("SavingsManager", chain)
+        const musdAddr = resolveAddress("mUSD", chain, "address")
+        const recipientAddr = "0x8f2A9Ce873AEBd7a212A942B12b2209Fa00831D2"
+        const briber = "0xd0f0F590585384AF7AB420bE1CFB3A3F8a82D775"
+
+        const savingsManager = SavingsManager__factory.connect(savingsManagerAddr, signer)
+        const musd = ERC20__factory.connect(musdAddr, signer)
+        const recipient = GaugeBriber__factory.connect(recipientAddr, signer)
+
+        // EXEC
+        const bal0 = await musd.balanceOf(briber)
+        console.log(bal0.toString())
+        // 1. Forward
+        let tx = await savingsManager.distributeUnallocatedInterest(musdAddr)
+        await tx.wait(2)
+        // 2. Distribute
+        tx = await recipient.forward()
+        await tx.wait(2)
+
+        // CHECK
+        const bal1 = await musd.balanceOf(briber)
+        console.log(bal1.toString())
     })
 
 module.exports = {}
