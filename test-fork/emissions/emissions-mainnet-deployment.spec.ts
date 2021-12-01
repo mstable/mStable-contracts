960bc5bd1a82e7bf601a487911334effb5244ea7
Nicholas Addison <nick@addisonbrown.com.au>
2021-12-01 19:32:56 +1100

    chore: fork test moved to after RAI vault deployed

--- test-fork/emissions/emissions-mainnet-deployment.spec.ts
+++ test-fork/emissions/emissions-mainnet-deployment.spec.ts
@@ -10,28 +10,30 @@ import { BN, simpleToExactAmount } from "@utils/math"
 import { currentWeekEpoch, increaseTime, increaseTimeTo } from "@utils/time"
 import { MAX_UINT256, ONE_WEEK } from "@utils/constants"
 import { assertBNClose } from "@utils/assertions"
-import { DAI, GUSD, HBTC, mBTC, MTA, mUSD, USDC, WBTC } from "tasks/utils/tokens"
+import { DAI, GUSD, HBTC, mBTC, MTA, mUSD, RAI, TBTCv2, USDC, WBTC } from "tasks/utils/tokens"
 import {
     BridgeForwarder,
     EmissionsController,
     IERC20,
     IERC20__factory,
     InitializableRewardsDistributionRecipient__factory,
+    IUniswapV3Quoter__factory,
     RevenueBuyBack,
     SavingsManager,
     SavingsManager__factory,
 } from "types/generated"
 import { Account } from "types/common"
 import { encodeUniswapPath } from "@utils/peripheral/uniswap"
+import { btcFormatter, usdFormatter } from "tasks/utils/quantity-formatters"
 
-const staker1VotingPower = BN.from("44461750008245826445414")
+const voter1VotingPower = BN.from("44461750008245826445414")
 
 context("Fork test Emissions Controller on mainnet", () => {
     let ops: Signer
     let governor: Signer
-    let staker1: Account
-    let staker2: Account
-    let staker3: Account
+    let voter1: Account
+    let voter2: Account
+    let voter3: Account
     let treasury: Account
     let emissionsController: EmissionsController
     let mta: IERC20
@@ -42,7 +44,7 @@ context("Fork test Emissions Controller on mainnet", () => {
                 {
                     forking: {
                         jsonRpcUrl: process.env.NODE_URL,
-                        blockNumber: 13636458,
+                        blockNumber: 13719000,
                     },
                 },
             ],
@@ -50,10 +52,10 @@ context("Fork test Emissions Controller on mainnet", () => {
         ops = await impersonate(resolveAddress("OperationsSigner"))
         governor = await impersonate(resolveAddress("Governor"))
         // 43,700 stkMTA, boosted to 44,461.750008245826445414 voting power
-        staker1 = await impersonateAccount("0x8d0f5678557192e23d1da1c689e40f25c063eaa5")
+        voter1 = await impersonateAccount("0x8d0f5678557192e23d1da1c689e40f25c063eaa5")
         // 27,527.5 stkMTA not boosted
-        staker2 = await impersonateAccount("0xa22fe318725a3858cf5ea4349802537798f0081a")
-        staker3 = await impersonateAccount("0x530deFD6c816809F54F6CfA6FE873646F6EcF930") // 82,538.415914215331337512 stkBPT
+        voter2 = await impersonateAccount("0xa22fe318725a3858cf5ea4349802537798f0081a")
+        voter3 = await impersonateAccount("0x530deFD6c816809F54F6CfA6FE873646F6EcF930") // 82,538.415914215331337512 stkBPT
         treasury = await impersonateAccount("0x3dd46846eed8d147841ae162c8425c08bd8e1b41")
 
         mta = IERC20__factory.connect(MTA.address, treasury.signer)
@@ -69,8 +71,9 @@ context("Fork test Emissions Controller on mainnet", () => {
             expect(await emissionsController.getDialRecipient(4), "dial 4 GUSD Vault").to.eq("0xAdeeDD3e5768F7882572Ad91065f93BA88343C99")
             expect(await emissionsController.getDialRecipient(5), "dial 5 BUSD Vault").to.eq("0xD124B55f70D374F58455c8AEdf308E52Cf2A6207")
             expect(await emissionsController.getDialRecipient(6), "dial 6 alUSD Vault").to.eq("0x0997dDdc038c8A958a3A3d00425C16f8ECa87deb")
-            expect(await emissionsController.getDialRecipient(7), "dial 7 tBTCv2 Vault").to.eq("0x97E2a2F97A2E9a4cFB462a49Ab7c8D205aBB9ed9")
+            expect(await emissionsController.getDialRecipient(7), "dial 7 RAI Vault").to.eq("0xF93e0ddE0F7C48108abbD880DB7697A86169f13b")
             expect(await emissionsController.getDialRecipient(8), "dial 8 HBTC Vault").to.eq("0xF65D53AA6e2E4A5f4F026e73cb3e22C22D75E35C")
+            expect(await emissionsController.getDialRecipient(9), "dial 9 tBTCv2 Vault").to.eq("0x97E2a2F97A2E9a4cFB462a49Ab7c8D205aBB9ed9")
 
             const dial0Data = await emissionsController.dials(0)
             expect(dial0Data.recipient, "dial 0 recipient").to.eq("0x8f2326316eC696F6d023E37A9931c2b2C177a3D7")
@@ -102,14 +105,14 @@ context("Fork test Emissions Controller on mainnet", () => {
             expect(await visorFinanceDial.endRecipient(), "Visor Finance Router").to.eq("0xF3f4F4e17cC65BDC36A36fDa5283F8D8020Ad0a4")
 
             const tx = await emissionsController.connect(governor).addDial(visorFinanceDial.address, 0, true)
-            await expect(tx).to.emit(emissionsController, "AddedDial").withArgs(9, visorFinanceDial.address)
+            await expect(tx).to.emit(emissionsController, "AddedDial").withArgs(10, visorFinanceDial.address)
 
-            expect(await emissionsController.getDialRecipient(9), "dial 9 VisorRouter Vault").to.eq(visorFinanceDial.address)
+            expect(await emissionsController.getDialRecipient(10), "dial 10 VisorRouter Vault").to.eq(visorFinanceDial.address)
 
-            const dial9Data = await emissionsController.dials(9)
-            expect(dial9Data.recipient, "dial 9 recipient").to.eq(visorFinanceDial.address)
-            expect(dial9Data.cap, "dial 9 cap").to.eq(0)
-            expect(dial9Data.notify, "dial 9 notify").to.eq(true)
+            const dial9Data = await emissionsController.dials(10)
+            expect(dial9Data.recipient, "dial 10 recipient").to.eq(visorFinanceDial.address)
+            expect(dial9Data.cap, "dial 10 cap").to.eq(0)
+            expect(dial9Data.notify, "dial 10 notify").to.eq(true)
         })
         it("Deploy bridgeForwarder for Polygon mUSD Vault", async () => {
             emissionsController = await deployEmissionsController(ops, hre)
@@ -125,18 +128,18 @@ context("Fork test Emissions Controller on mainnet", () => {
 
             const tx = await emissionsController.connect(governor).addDial(bridgeForwarder.address, 0, true)
 
-            await expect(tx).to.emit(emissionsController, "AddedDial").withArgs(9, bridgeForwarder.address)
+            await expect(tx).to.emit(emissionsController, "AddedDial").withArgs(10, bridgeForwarder.address)
 
-            expect(await emissionsController.getDialRecipient(9), "dial 9 Bridge Forwarder").to.eq(bridgeForwarder.address)
+            expect(await emissionsController.getDialRecipient(10), "dial 10 Bridge Forwarder").to.eq(bridgeForwarder.address)
         })
     })
     describe("Set vote weights", () => {
         before(async () => {
             emissionsController = await deployEmissionsController(ops, hre)
         })
-        it("staker 1", async () => {
-            expect(await emissionsController.callStatic.getVotes(staker1.address), "staker 1 total voting power").to.eq(staker1VotingPower)
-            const tx = await emissionsController.connect(staker1.signer).setVoterDialWeights([
+        it("voter 1", async () => {
+            expect(await emissionsController.callStatic.getVotes(voter1.address), "voter 1 total voting power").to.eq(voter1VotingPower)
+            const tx = await emissionsController.connect(voter1.signer).setVoterDialWeights([
                 {
                     dialId: 0,
                     weight: 120, // 60%
@@ -148,11 +151,11 @@ context("Fork test Emissions Controller on mainnet", () => {
             ])
             await expect(tx).to.emit(emissionsController, "PreferencesChanged")
         })
-        it("staker 2", async () => {
-            expect(await emissionsController.callStatic.getVotes(staker2.address), "staker 2 total voting power").to.eq(
+        it("voter 2", async () => {
+            expect(await emissionsController.callStatic.getVotes(voter2.address), "voter 2 total voting power").to.eq(
                 simpleToExactAmount(27527.5),
             )
-            const tx = await emissionsController.connect(staker2.signer).setVoterDialWeights([
+            const tx = await emissionsController.connect(voter2.signer).setVoterDialWeights([
                 {
                     dialId: 2,
                     weight: 200, // 100%
@@ -160,11 +163,11 @@ context("Fork test Emissions Controller on mainnet", () => {
             ])
             await expect(tx).to.emit(emissionsController, "PreferencesChanged")
         })
-        it("staker 3", async () => {
-            expect(await emissionsController.callStatic.getVotes(staker3.address), "staker 3 total voting power").to.eq(
-                "82538415914215331337512",
+        it("voter 3", async () => {
+            expect(await emissionsController.callStatic.getVotes(voter3.address), "voter 3 total voting power").to.eq(
+                "78211723319712171214037",
             )
-            const tx = await emissionsController.connect(staker3.signer).setVoterDialWeights([
+            const tx = await emissionsController.connect(voter3.signer).setVoterDialWeights([
                 {
                     dialId: 1,
                     weight: 100, // 100%
@@ -184,7 +187,7 @@ context("Fork test Emissions Controller on mainnet", () => {
             const bridgeForwarder = await deployBridgeForwarder(ops, hre, bridgeRecipient.address, emissionsController.address)
             await emissionsController.connect(governor).addDial(bridgeForwarder.address, 0, true)
 
-            await emissionsController.connect(staker1.signer).setVoterDialWeights([
+            await emissionsController.connect(voter1.signer).setVoterDialWeights([
                 {
                     dialId: 0,
                     weight: 10, // 5% which is under the 10% cap
@@ -225,7 +228,7 @@ context("Fork test Emissions Controller on mainnet", () => {
             // expect(totalREwardsActual, "total rewards").to.eq(totalRewardsExpected)
             assertBNClose(totalREwardsActual, totalRewardsExpected, 2, "total rewards")
 
-            expect(distributionAmounts, "number of dials").to.lengthOf(11)
+            expect(distributionAmounts, "number of dials").to.lengthOf(12)
             // Dial over cap so get 10% of the distribution
             const dial1AmountExpected = totalRewardsExpected.mul(10).div(100)
             assertBNClose(distributionAmounts[1], dial1AmountExpected, 100, "dial 1 amount")
@@ -261,7 +264,7 @@ context("Fork test Emissions Controller on mainnet", () => {
             await emissionsController
                 .connect(treasury.signer)
                 .donate(
-                    [0, 1, 2, 3, 8, 9, 10],
+                    [0, 1, 2, 3, 9, 10, 11],
                     [
                         simpleToExactAmount(100),
                         simpleToExactAmount(1000),
@@ -278,7 +281,9 @@ context("Fork test Emissions Controller on mainnet", () => {
             await setRewardsDistribution(mUSD.vault)
             await setRewardsDistribution(mBTC.vault)
             await setRewardsDistribution(GUSD.vault)
+            await setRewardsDistribution(RAI.vault)
             await setRewardsDistribution(HBTC.vault)
+            await setRewardsDistribution(TBTCv2.vault)
         })
         it("distribute rewards to staking contracts", async () => {
             const tx = await emissionsController.distributeRewards([0, 1])
@@ -286,10 +291,10 @@ context("Fork test Emissions Controller on mainnet", () => {
             await expect(tx).to.emit(emissionsController, "DistributedReward").withArgs(1, simpleToExactAmount(1000))
         })
         it("distribute rewards to vaults", async () => {
-            const tx = await emissionsController.distributeRewards([2, 3, 4, 8])
+            const tx = await emissionsController.distributeRewards([2, 3, 4, 9])
             await expect(tx).to.emit(emissionsController, "DistributedReward").withArgs(2, simpleToExactAmount(2000))
             await expect(tx).to.emit(emissionsController, "DistributedReward").withArgs(3, simpleToExactAmount(3000))
-            await expect(tx).to.emit(emissionsController, "DistributedReward").withArgs(8, simpleToExactAmount(8000))
+            await expect(tx).to.emit(emissionsController, "DistributedReward").withArgs(9, simpleToExactAmount(8000))
         })
         it("distribute rewards to bridge forwarder", async () => {
             const bridgeTokenLockerAddress = resolveAddress("PolygonPoSBridge")
@@ -301,10 +306,10 @@ context("Fork test Emissions Controller on mainnet", () => {
             expect(await mta.balanceOf(bridgeForwarder.address), "bridge forwarder MAT bal before").to.eq(0)
 
             // Distribute rewards
-            const tx = await emissionsController.distributeRewards([10])
+            const tx = await emissionsController.distributeRewards([11])
 
             // Check events
-            await expect(tx).to.emit(emissionsController, "DistributedReward").withArgs(10, bridgeAmount)
+            await expect(tx).to.emit(emissionsController, "DistributedReward").withArgs(11, bridgeAmount)
             await expect(tx).to.emit(bridgeForwarder, "Forwarded").withArgs(bridgeAmount)
 
             // Check MTA balances
@@ -344,6 +349,24 @@ context("Fork test Emissions Controller on mainnet", () => {
             emissionsController = await deployEmissionsController(ops, hre)
             mtaToken = IERC20__factory.connect(MTA.address, ops)
             revenueBuyBack = await deployRevenueBuyBack(ops, hre, emissionsController.address)
+        })
+        it("check Uniswap USDC to MTA price", async () => {
+            const uniswapQuoterAddress = resolveAddress("UniswapQuoterV3")
+            const uniswapQuoter = IUniswapV3Quoter__factory.connect(uniswapQuoterAddress, ops)
+            const mtaAmount = await uniswapQuoter.callStatic.quoteExactInput(
+                musdUniswapPath.encoded,
+                simpleToExactAmount(10000, USDC.decimals),
+            )
+            console.log(`${usdFormatter(mtaAmount)} MTA (${usdFormatter(mtaAmount.div(10000))} USDC/MTA)`)
+        })
+        it("check Uniswap WBTC to MTA price", async () => {
+            const uniswapQuoterAddress = resolveAddress("UniswapQuoterV3")
+            const uniswapQuoter = IUniswapV3Quoter__factory.connect(uniswapQuoterAddress, ops)
+            const wbtcMtaPrice = await uniswapQuoter.callStatic.quoteExactInput(
+                mbtcUniswapPath.encoded,
+                simpleToExactAmount(0.05, WBTC.decimals),
+            )
+            console.log(`${btcFormatter(wbtcMtaPrice)} MTA (${usdFormatter(wbtcMtaPrice.div(2))} WBTC/MTA)`)
         })
         it("Post deploy checks", async () => {
             expect(await revenueBuyBack.REWARDS_TOKEN()).to.eq(MTA.address)
@@ -393,7 +416,7 @@ context("Fork test Emissions Controller on mainnet", () => {
                         mUSD.address,
                         USDC.address,
                         simpleToExactAmount(98, 4),
-                        simpleToExactAmount(54, 28), // 0.54 USDC/MTA
+                        simpleToExactAmount(66, 28), // min 0.66 USDC for 1 MTA to 30 decimal places
                         musdUniswapPath.encoded,
                     )
                     const tx = revenueBuyBack.buyBackRewards([mUSD.address])
@@ -407,15 +430,13 @@ context("Fork test Emissions Controller on mainnet", () => {
                     await savingsManager.distributeUnallocatedInterest(mBTC.address)
                 })
                 it("as minMasset2BassetPrice is too high", async () => {
-                    await revenueBuyBack
-                        .connect(governor)
-                        .setMassetConfig(
-                            mBTC.address,
-                            WBTC.address,
-                            simpleToExactAmount(101, 6),
-                            simpleToExactAmount(50, 13),
-                            mbtcUniswapPath.encoded,
-                        )
+                    await revenueBuyBack.connect(governor).setMassetConfig(
+                        mBTC.address,
+                        WBTC.address,
+                        simpleToExactAmount(101, 6), // 1.01
+                        simpleToExactAmount(50, 13),
+                        mbtcUniswapPath.encoded,
+                    )
                     const tx = revenueBuyBack.buyBackRewards([mBTC.address])
                     await expect(tx).to.revertedWith("bAsset qty < min qty")
                     expect(await mtaToken.balanceOf(revenueBuyBack.address), "RevenueBuyBack MTA bal after").to.eq(0)
