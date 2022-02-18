b24b3b78b090efa479085e556b7a24d99a4c5be6
doncesarts <doncesarts@gmail.com>
2022-02-18 01:38:40 -0600

    feat: adds referrer to SaveWrapper deposits

--- contracts/savings/peripheral/SaveWrapper.sol
+++ contracts/savings/peripheral/SaveWrapper.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 pragma solidity 0.8.6;
 
-import { ISavingsContractV2 } from "../../interfaces/ISavingsContract.sol";
+import { ISavingsContractV3 } from "../../interfaces/ISavingsContract.sol";
 import { IMasset } from "../../interfaces/IMasset.sol";
 import { IFeederPool } from "../../interfaces/IFeederPool.sol";
 import { IBoostedVaultWithLockup } from "../../interfaces/IBoostedVaultWithLockup.sol";
@@ -46,6 +46,32 @@ contract SaveWrapper is Ownable {
     }
 
     /**
+     * @dev 0. Simply saves an mAsset and then into the vault
+     * @param _mAsset   mAsset address
+     * @param _save     Save address
+     * @param _vault    Boosted Savings Vault address
+     * @param _amount   Units of mAsset to deposit to savings
+     * @param _referrer Referrer address for this deposit.
+     */
+    function saveAndStake(
+        address _mAsset,
+        address _save,
+        address _vault,
+        uint256 _amount,
+        address _referrer
+    ) external {
+        require(_mAsset != address(0), "Invalid mAsset");
+        require(_save != address(0), "Invalid save");
+        require(_vault != address(0), "Invalid vault");
+
+        // 1. Get the input mAsset
+        IERC20(_mAsset).safeTransferFrom(msg.sender, address(this), _amount);
+
+        // 2. Mint imAsset and stake in vault
+        _saveAndStake(_save, _vault, _amount, _referrer, true);
+    }
+
+    /**
      * @dev 1. Mints an mAsset and then deposits to Save/Savings Vault
      * @param _mAsset       mAsset address
      * @param _bAsset       bAsset address
@@ -77,6 +103,42 @@ contract SaveWrapper is Ownable {
 
         // 3. Mint imAsset and optionally stake in vault
         _saveAndStake(_save, _vault, massetsMinted, _stake);
+    }
+
+    /**
+     * @dev 1. Mints an mAsset and then deposits to Save/Savings Vault
+     * @param _mAsset       mAsset address
+     * @param _bAsset       bAsset address
+     * @param _save         Save address
+     * @param _vault        Boosted Savings Vault address
+     * @param _amount       Amount of bAsset to mint with
+     * @param _minOut       Min amount of mAsset to get back
+     * @param _stake        Add the imAsset to the Boosted Savings Vault?
+     * @param _referrer     Referrer address for this deposit.
+     */
+    function saveViaMint(
+        address _mAsset,
+        address _save,
+        address _vault,
+        address _bAsset,
+        uint256 _amount,
+        uint256 _minOut,
+        bool _stake,
+        address _referrer
+    ) external {
+        require(_mAsset != address(0), "Invalid mAsset");
+        require(_save != address(0), "Invalid save");
+        require(_vault != address(0), "Invalid vault");
+        require(_bAsset != address(0), "Invalid bAsset");
+
+        // 1. Get the input bAsset
+        IERC20(_bAsset).safeTransferFrom(msg.sender, address(this), _amount);
+
+        // 2. Mint
+        uint256 massetsMinted = IMasset(_mAsset).mint(_bAsset, _amount, _minOut, address(this));
+
+        // 3. Mint imAsset and optionally stake in vault
+        _saveAndStake(_save, _vault, massetsMinted, _referrer, _stake);
     }
 
     /**
@@ -120,6 +182,51 @@ contract SaveWrapper is Ownable {
 
         // 2. Deposit the mAsset into Save and optionally stake in the vault
         _saveAndStake(_save, _vault, mAssetQuantity, _stake);
+    }
+
+    /**
+     * @dev 2. Swaps fAsset for mAsset and then deposits to Save/Savings Vault
+     * @param _mAsset             mAsset address
+     * @param _save               Save address
+     * @param _vault              Boosted Savings Vault address
+     * @param _feeder             Feeder Pool address
+     * @param _fAsset             fAsset address
+     * @param _fAssetQuantity     Quantity of fAsset sent
+     * @param _minOutputQuantity  Min amount of mAsset to be swapped and deposited
+     * @param _stake              Deposit the imAsset in the Savings Vault?
+     * @param _referrer       Referrer address for this deposit.
+     */
+    function saveViaSwap(
+        address _mAsset,
+        address _save,
+        address _vault,
+        address _feeder,
+        address _fAsset,
+        uint256 _fAssetQuantity,
+        uint256 _minOutputQuantity,
+        bool _stake,
+        address _referrer
+    ) external {
+        require(_feeder != address(0), "Invalid feeder");
+        require(_mAsset != address(0), "Invalid mAsset");
+        require(_save != address(0), "Invalid save");
+        require(_vault != address(0), "Invalid vault");
+        require(_fAsset != address(0), "Invalid input");
+
+        // 0. Transfer the fAsset here
+        IERC20(_fAsset).safeTransferFrom(msg.sender, address(this), _fAssetQuantity);
+
+        // 1. Swap the fAsset for mAsset with the feeder pool
+        uint256 mAssetQuantity = IFeederPool(_feeder).swap(
+            _fAsset,
+            _mAsset,
+            _fAssetQuantity,
+            _minOutputQuantity,
+            address(this)
+        );
+
+        // 2. Deposit the mAsset into Save and optionally stake in the vault
+        _saveAndStake(_save, _vault, mAssetQuantity, _referrer, _stake);
     }
 
     /**
@@ -167,6 +274,52 @@ contract SaveWrapper is Ownable {
     }
 
     /**
+     * @dev 3. Buys a bAsset on Uniswap with ETH, then mints imAsset via mAsset,
+     *         optionally staking in the Boosted Savings Vault
+     * @param _mAsset         mAsset address
+     * @param _save           Save address
+     * @param _vault          Boosted vault address
+     * @param _uniswap        Uniswap router address
+     * @param _amountOutMin   Min uniswap output in bAsset units
+     * @param _path           Sell path on Uniswap (e.g. [WETH, DAI])
+     * @param _minOutMStable  Min amount of mAsset to receive
+     * @param _stake          Add the imAsset to the Savings Vault?
+     * @param _referrer       Referrer address for this deposit.
+     */
+    function saveViaUniswapETH(
+        address _mAsset,
+        address _save,
+        address _vault,
+        address _uniswap,
+        uint256 _amountOutMin,
+        address[] calldata _path,
+        uint256 _minOutMStable,
+        bool _stake,
+        address _referrer
+    ) external payable {
+        require(_mAsset != address(0), "Invalid mAsset");
+        require(_save != address(0), "Invalid save");
+        require(_vault != address(0), "Invalid vault");
+        require(_uniswap != address(0), "Invalid uniswap");
+
+        // 1. Get the bAsset
+        uint256[] memory amounts = IUniswapV2Router02(_uniswap).swapExactETHForTokens{
+            value: msg.value
+        }(_amountOutMin, _path, address(this), block.timestamp + 1000);
+
+        // 2. Purchase mAsset
+        uint256 massetsMinted = IMasset(_mAsset).mint(
+            _path[_path.length - 1],
+            amounts[amounts.length - 1],
+            _minOutMStable,
+            address(this)
+        );
+
+        // 3. Mint imAsset and optionally stake in vault
+        _saveAndStake(_save, _vault, massetsMinted, _referrer, _stake);
+    }
+
+    /**
      * @dev Gets estimated mAsset output from a WETH > bAsset > mAsset trade
      * @param _mAsset       mAsset address
      * @param _uniswap      Uniswap router address
@@ -199,10 +352,36 @@ contract SaveWrapper is Ownable {
         bool _stake
     ) internal {
         if (_stake) {
-            uint256 credits = ISavingsContractV2(_save).depositSavings(_amount, address(this));
+            uint256 credits = ISavingsContractV3(_save).depositSavings(_amount, address(this));
             IBoostedVaultWithLockup(_vault).stake(msg.sender, credits);
         } else {
-            ISavingsContractV2(_save).depositSavings(_amount, msg.sender);
+            ISavingsContractV3(_save).depositSavings(_amount, msg.sender);
+        }
+    }
+
+    /** @dev Internal func to deposit into Save and optionally stake in the vault
+     * @param _save       Save address
+     * @param _vault      Boosted vault address
+     * @param _amount     Amount of mAsset to deposit
+     * @param _stake      Add the imAsset to the Savings Vault?
+     * @param _referrer   Referrer address for this deposit.
+     */
+    function _saveAndStake(
+        address _save,
+        address _vault,
+        uint256 _amount,
+        address _referrer,
+        bool _stake
+    ) internal {
+        if (_stake) {
+            uint256 credits = ISavingsContractV3(_save).depositSavings(
+                _amount,
+                address(this),
+                _referrer
+            );
+            IBoostedVaultWithLockup(_vault).stake(msg.sender, credits);
+        } else {
+            ISavingsContractV3(_save).depositSavings(_amount, msg.sender, _referrer);
         }
     }
 
