5b797044100afec440c2c8c69c762c42bd809b95
doncesarts <doncesarts@gmail.com>
2022-04-07 03:14:36 +0100

    feat: implements 4626 on SavingsContract

--- contracts/interfaces/ISavingsContract.sol
+++ contracts/interfaces/ISavingsContract.sol
@@ -46,36 +46,27 @@ interface ISavingsContractV2 {
     function underlying() external view returns (IERC20 underlyingMasset); // V2
 }
 
-interface ISavingsContractV3 is IERC4626Vault {
+interface ISavingsContractV3 {
     // DEPRECATED but still backwards compatible
     function redeem(uint256 _amount) external returns (uint256 massetReturned);
 
     function creditBalances(address) external view returns (uint256); // V1 & V2 (use balanceOf)
 
-    /*///////////////////////////////////////////////////////////////
-                DEPRECATED for  IERC4626Vault
-    //////////////////////////////////////////////////////////////*/
-    /** @dev Deprecated in favour of  IERC4626Vault.deposit(uint256 assets, address receiver)*/
+    // --------------------------------------------
+    function depositInterest(uint256 _amount) external; // V1 & V2
+
+    /** @dev see IERC4626Vault.deposit(uint256 assets, address receiver)*/
     function depositSavings(uint256 _amount) external returns (uint256 creditsIssued); // V1 & V2
 
+    /** @dev see IERC4626Vault.deposit(uint256 assets, address receiver)*/
     function depositSavings(uint256 _amount, address _beneficiary)
         external
         returns (uint256 creditsIssued); // V2
 
+    /** @dev see IERC4626Vault.redeem(uint256 shares,address receiver,address owner)(uint256 assets);*/
     function redeemCredits(uint256 _amount) external returns (uint256 underlyingReturned); // V2
 
-    /** @dev Deprecated in favour of  IERC4626Vault.withdraw(uint256 assets,address receiver,address owner)(uint256 assets, address receiver)*/
-    function redeemUnderlying(uint256 _amount) external returns (uint256 creditsBurned); // V2
-
-    /** @dev Deprecated in favour of  IERC4626Vault.assetsPerShare() external view returns (uint256 assetsPerUnitShare);*/
-    function exchangeRate() external view returns (uint256); // V1 & V2
-
-    /** @dev Deprecated in favour of  IERC4626Vault.assetsOf(addresss depositor) view returns (uint256 assets)*/
-    function balanceOfUnderlying(address _user) external view returns (uint256 underlying); // V2
-
-    /** @dev Deprecated in favour of  IERC4626Vault.asset()(address assetTokenAddress);*/
-    function underlying() external view returns (IERC20 underlyingMasset); // V2
-
+    /** @dev see IERC4626Vault.withdraw(uint256 assets,address receiver,address owner)(uint256 assets, address receiver)*/
     function redeemUnderlying(uint256 _amount) external returns (uint256 creditsBurned); // V2
 
     function exchangeRate() external view returns (uint256); // V1 & V2
@@ -86,14 +77,10 @@ interface ISavingsContractV3 is IERC4626Vault {
 
     function creditsToUnderlying(uint256 _credits) external view returns (uint256 underlying); // V2
 
+    /** @dev see IERC4626Vault.asset()(address assetTokenAddress);*/
     function underlying() external view returns (IERC20 underlyingMasset); // V2
 
     // --------------------------------------------
-    function deposit(
-        uint256 assets,
-        address receiver,
-        address referrer
-    ) external returns (uint256 creditsIssued); // V3
 
     function redeemAndUnwrap(
         uint256 _amount,
@@ -109,11 +96,11 @@ interface ISavingsContractV3 is IERC4626Vault {
             uint256 creditsBurned,
             uint256 massetRedeemed,
             uint256 outputQuantity
-        );
+        ); // V3
 
     function depositSavings(
         uint256 _underlying,
         address _beneficiary,
         address _referrer
-    ) external returns (uint256 creditsIssued);
+    ) external returns (uint256 creditsIssued); // V3
 }
