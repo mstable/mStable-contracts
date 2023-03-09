b45b544a47c7ab0a9599187f8dbe12dc1841c726
doncesarts <doncesarts@gmail.com>
2023-03-09 10:59:33 +0000

    feat: adds mta redeemer

--- test/shared/meta-token-redeemer.spec.ts
+++ test/shared/meta-token-redeemer.spec.ts
@@ -1,6 +1,6 @@
 import { BN, simpleToExactAmount } from "@utils/math"
 import { ethers } from "hardhat"
-import { ERC20, MetaTokenRedeemer, MetaTokenRedeemer__factory, MockERC20__factory, MockRoot__factory } from "types/generated"
+import { ERC20, MetaTokenRedeemer, MetaTokenRedeemer__factory, MockERC20__factory } from "types/generated"
 import { expect } from "chai"
 import { Signer } from "ethers"
 import { ZERO } from "@utils/constants"
@@ -51,29 +51,29 @@ describe("MetaTokenRedeemer", () => {
         const redeemerWethBalanceBefore = await weth.balanceOf(redeemer.address)
 
         const amount = aliceBalanceBefore.div(2)
-        const wethAmount = amount.mul(rate).div(simpleToExactAmount(1))
+        const expectedWeth = amount.mul(rate).div(simpleToExactAmount(1))
 
         expect(aliceBalanceBefore, "balance").to.be.gt(ZERO)
         await mta.connect(alice).approve(redeemer.address, ethers.constants.MaxUint256)
 
         const tx1 = await redeemer.connect(alice).redeem(amount)
-        expect(tx1).to.emit(redeemer, "Redeemed").withArgs(aliceAddress, amount, wethAmount)
+        expect(tx1).to.emit(redeemer, "Redeemed").withArgs(aliceAddress, amount, expectedWeth)
 
         const tx2 = await redeemer.connect(alice).redeem(amount)
-        expect(tx2).to.emit(redeemer, "Redeemed").withArgs(aliceAddress, amount, wethAmount)
+        expect(tx2).to.emit(redeemer, "Redeemed").withArgs(aliceAddress, amount, expectedWeth)
 
         const aliceBalanceAfter = await mta.balanceOf(aliceAddress)
         const aliceWethBalanceAfter = await weth.balanceOf(aliceAddress)
         const redeemerWethBalanceAfter = await weth.balanceOf(redeemer.address)
 
         expect(aliceBalanceAfter, "alice mta balance").to.be.eq(ZERO)
-        expect(aliceWethBalanceAfter, "alice weth balance").to.be.eq(aliceWethBalanceBefore.add(wethAmount.mul(2)))
-        expect(redeemerWethBalanceAfter, "redeemer weth balance").to.be.eq(redeemerWethBalanceBefore.sub(wethAmount.mul(2)))
+        expect(aliceWethBalanceAfter, "alice weth balance").to.be.eq(aliceWethBalanceBefore.add(expectedWeth.mul(2)))
+        expect(redeemerWethBalanceAfter, "redeemer weth balance").to.be.eq(redeemerWethBalanceBefore.sub(expectedWeth.mul(2)))
     })
     it("fails if there is not enough WETH (non realistic example) ", async () => {
         const mtaAmount = await mta.balanceOf(await deployer.getAddress())
-        const wethAmount = mtaAmount.mul(rate).div(simpleToExactAmount(1))
-        expect(wethAmount).to.be.gt(simpleToExactAmount(20))
+        const expectedWeth = mtaAmount.mul(rate).div(simpleToExactAmount(1))
+        expect(expectedWeth).to.be.gt(simpleToExactAmount(20))
         await mta.approve(redeemer.address, mtaAmount)
 
         await expect(redeemer.redeem(mtaAmount)).to.be.revertedWith("not enough WETH")
